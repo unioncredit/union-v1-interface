06ebf2f785e1ef60520086074bad2f5f11e33ec0
Jeff Reiner <theundeadghost@gmail.com>
2020-03-10 13:55:06 +0100

    :sparkles: Adding CONTRACT_ADDRESSES and AddressZero for now
    Will need to update

---
+++ constants/index.js
@@ -0,0 +1,9 @@
+export const CONTRACT_ADDRESSES = {
+  1: AddressZero,
+  3: AddressZero,
+  4: AddressZero,
+  5: AddressZero,
+  42: AddressZero
+};
+
+export const AddressZero = "0x0000000000000000000000000000000000000000";
