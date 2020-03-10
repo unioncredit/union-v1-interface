71c89ecd8db103033281e5a2b07566377e5a2a96
Jeff Reiner <theundeadghost@gmail.com>
2020-03-10 14:14:38 +0100

    :sparkles: Adding Web3ReactProvider to _app

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import { Web3ReactProvider } from "@web3-react/core";
+import getLibrary from "../lib/getLibrary";
+
+export default function UnionApp({ Component, pageProps }) {
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,27 @@
 import Head from "next/head";
 
-const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
 
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
-);
-
-export default Home;
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
