51af3f0747f662074060b403f4fb4a041fb9ca41
Jeff Reiner <theundeadghost@gmail.com>
2020-03-16 20:41:43 +0100

    Refactor to use postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -8,9 +8,8 @@ const purgecss = [
 
 module.exports = {
   plugins: [
-    "postcss-import",
     "tailwindcss",
-    "autoprefixer",
+    "postcss-preset-env",
     ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
 };
