dca8e13be19bcd641724ff2a2ef923260ec4e65f
Jeff Reiner <theundeadghost@gmail.com>
2020-03-16 18:24:43 +0100

    :sparkles: Preloading font

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,35 @@ import Navigation from "@components/navigation";
 import getLibrary from "@lib/getLibrary";
 import { Web3ReactProvider } from "@web3-react/core";
 import "../css/tailwind.css";
+import Head from "next/head";
 
 export default function UnionApp({ Component, pageProps }) {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
+      <Head>
+        <link
+          rel="preload"
+          href="/Montserrat-400.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/Montserrat-500.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/Montserrat-600.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+      </Head>
+
       <header>
         <Navigation />
       </header>
