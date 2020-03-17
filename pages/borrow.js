4c2c76483e2c2953ec0a90f0f52ed1a7ee26b7b5
Jeff Reiner <theundeadghost@gmail.com>
2020-03-17 19:10:33 +0100

    :sparkles: Stub in borrow page

--- pages/borrow.js
+++ pages/borrow.js
@@ -1,5 +1,6 @@
-import Head from "next/head";
+import Button from "@components/button";
 import Container from "@components/container";
+import Head from "next/head";
 
 export default function Borrow() {
   return (
@@ -10,7 +11,58 @@ export default function Borrow() {
       </Head>
 
       <Container>
-        <h1>Borrow</h1>
+        <div className="mb-5">
+          <h1>Dashboard</h1>
+        </div>
+
+        <div className="flex -mx-3">
+          <div className="w-1/2 px-3">
+            <div className="bg-blue-800 border border-blue-800 rounded p-6">
+              <div className="flex justify-between">
+                <div>
+                  <p>Available Credit</p>
+                  <p className="font-semibold">1520 DAI</p>
+                </div>
+
+                <Button type="tertiary">Borrow</Button>
+              </div>
+
+              <div className="flex justify-between">
+                <p>Percent Utilization</p>
+                <div className="flex">
+                  <progress value={66.99} max={100} />
+                  <p className="font-semibold">66.99%</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-1/2 px-3">
+            <div className="bg-white border border-grey-200 rounded p-6">
+              <div className="flex justify-between">
+                <div>
+                  <p>Balance Owed</p>
+                  <p className="font-semibold">693.34 DAI</p>
+                </div>
+
+                <Button>Repay</Button>
+              </div>
+
+              <div className="flex justify-between">
+                <p>Minimum Payment Due</p>
+                <p className="font-semibold">64.28 DAI</p>
+              </div>
+
+              <div className="flex justify-between">
+                <p>Payment Due Date</p>
+                <p className="font-semibold">in 10 Days</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mb-5">
+          <h2>Transactions</h2>
+        </div>
       </Container>
     </div>
   );
