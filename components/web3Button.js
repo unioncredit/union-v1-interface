6def831e2d384251f226e5a64b0a2e61c2a24843
Jeff Reiner <theundeadghost@gmail.com>
2020-03-18 18:32:03 +0100

    :art: better conditional with error prop

--- components/web3Button.js
+++ components/web3Button.js
@@ -1,11 +1,11 @@
 import classNames from "classnames";
 
-const Web3Button = ({ error, ...props }) => {
+const Web3Button = ({ error = false, ...props }) => {
   const cachedClassNames = classNames(
     "font-medium px-4 py-3 rounded border leading-snug transition-colors duration-200",
-    error
-      ? "border-red-700 bg-red-500 text-white"
-      : "border-gray-200 bg-gray-100 hover:bg-gray-300 hover:border-gray-400"
+    !error
+      ? "bg-gray-100 border-gray-200 hover:bg-gray-300 hover:border-gray-400"
+      : "bg-red-500 border-red-700 hover:bg-red-700 hover:border-red-900 text-white"
   );
 
   return <button className={cachedClassNames} {...props} />;
