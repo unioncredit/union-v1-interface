638eda555cc41d50f079d84aa9133b1bafc2dbcd
Jeff Reiner <theundeadghost@gmail.com>
2020-03-18 12:17:04 +0100

    :art: improving conditional Container

--- components/container.js
+++ components/container.js
@@ -1,10 +1,11 @@
 import classNames from "classnames";
 
 const Container = ({ children, className, wide = false }) => {
-  const cachedClassNames = classNames(className, "w-full mx-auto", {
-    "max-w-screen-lg-gutter": !wide,
-    "max-w-screen-xl-gutter": wide
-  });
+  const cachedClassNames = classNames(
+    className,
+    "w-full mx-auto",
+    `max-w-screen-${wide ? "xl" : "lg"}-gutter`
+  );
 
   return <div className={cachedClassNames}>{children}</div>;
 };
