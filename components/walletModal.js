3e6877ebf3665607154f034d8bfb26d9ba5585c5
Jeff Reiner <theundeadghost@gmail.com>
2020-03-19 15:48:19 +0100

    :sparkles: Stubbing in bits for walletmodal

--- components/walletModal.js
+++ components/walletModal.js
@@ -3,6 +3,7 @@ import {
   useWalletModalToggle
 } from "@contexts/Application";
 import Modal from "./modal";
+import Button from "./button";
 
 const WalletModal = () => {
   const walletModalOpen = useWalletModalOpen();
@@ -12,12 +13,37 @@ const WalletModal = () => {
     <Modal isOpen={walletModalOpen} onDismiss={toggleWalletModal}>
       <div className="mb-8">
         <p className="text-center text-xl mb-3">Connect your wallet</p>
-        <div className="divider" />
+        <div className="divider h-1 bg-primary-500" />
         <p className="text-center leading-tight mt-6">
           As a final step your wallet will ask you to authorize UNION to access
           your account.
         </p>
       </div>
+
+      <div className="mt-8 mb-10">
+        <div className="mt-4">
+          <Button full type="invert">
+            MetaMask
+          </Button>
+        </div>
+        <div className="mt-4">
+          <Button full type="invert">
+            WalletConnect
+          </Button>
+        </div>
+        <div className="mt-4">
+          <Button full type="invert">
+            Fortmatic
+          </Button>
+        </div>
+      </div>
+
+      <div className="divider bg-gray-300" />
+
+      <p className="text-sm text-center mt-6">
+        Don't have an account?{" "}
+        <button className="underline font-medium">Start now</button>
+      </p>
     </Modal>
   );
 };
