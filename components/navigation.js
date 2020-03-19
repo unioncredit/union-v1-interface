ceb76fbc5e40f0b972b5a5ef1252830c69550479
Jeff Reiner <theundeadghost@gmail.com>
2020-03-19 19:37:52 +0100

    :sparkles: Adding more style and a bottom margin to the nav

--- components/navigation.js
+++ components/navigation.js
@@ -5,9 +5,9 @@ import Web3Status from "./web3Connection";
 
 const Navigation = () => {
   return (
-    <nav className="border-b">
+    <nav className="border-b mb-10">
       <Container wide>
-        <ul>
+        <ul className="flex items-center justify-between">
           <li>
             <Link href="/">
               <a>
@@ -15,33 +15,43 @@ const Navigation = () => {
               </a>
             </Link>
           </li>
-          <li>
-            <Link href="/vouch">
-              <a className="text-lg font-semibold">Vouch</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/borrow">
-              <a className="text-lg font-semibold">Borrow</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/stake">
-              <a className="text-lg font-semibold">Stake</a>
-            </Link>
-          </li>
-          <li>
-            <span
-              className="leading-none text-2xl"
-              role="img"
-              aria-label="Notification Bell"
-            >
-              🔔
-            </span>
-          </li>
-          <li>
-            <Web3Status />
-          </li>
+          <ul className="flex items-center">
+            <li>
+              <Link href="/vouch">
+                <a className="p-3 leading-none mx-4 text-lg font-semibold">
+                  Vouch
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/borrow">
+                <a className="p-3 leading-none mx-4 text-lg font-semibold">
+                  Borrow
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stake">
+                <a className="p-3 leading-none mx-4 text-lg font-semibold">
+                  Stake
+                </a>
+              </Link>
+            </li>
+          </ul>
+          <ul className="flex items-center">
+            <li>
+              <span
+                className="leading-none text-2xl"
+                role="img"
+                aria-label="Notification Bell"
+              >
+                🔔
+              </span>
+            </li>
+            <li>
+              <Web3Status />
+            </li>
+          </ul>
         </ul>
       </Container>
     </nav>
