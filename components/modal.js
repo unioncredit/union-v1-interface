3cb7085402bc613ddb5343a2631b82d0cac151b7
Jeff Reiner <theundeadghost@gmail.com>
2020-03-20 16:14:37 +0100

    :sparkles: Adding ModalHeader component

--- components/modal.js
+++ components/modal.js
@@ -1,5 +1,21 @@
 import { DialogContent, DialogOverlay } from "@reach/dialog";
 import PropTypes from "prop-types";
+import VisuallyHidden from "@reach/visually-hidden";
+
+export const ModalHeader = ({ title, onDismiss }) => (
+  <div className="px-4 py-6 sm:px-6 border-b relative">
+    <p className="text-center leading-none">{title}</p>
+    <button
+      className="focus:outline-none focus:shadow-outline leading-none p-2 rounded absolute inline-flex right-0 top-0 mr-6 mt-4"
+      onClick={onDismiss}
+    >
+      <VisuallyHidden>Close</VisuallyHidden>
+      <span aria-hidden className="w-4" role="img">
+        ❌
+      </span>
+    </button>
+  </div>
+);
 
 const Modal = ({ isOpen, onDismiss, children, label = "Modal" }) => {
   return (
