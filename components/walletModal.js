731a908448882e246e9681b665bbe24748d94d09
Jeff Reiner <theundeadghost@gmail.com>
2020-03-20 16:00:08 +0100

    :art: Moving padding from modal content to handled by the Modal itself

--- components/walletModal.js
+++ components/walletModal.js
@@ -11,39 +11,41 @@ const WalletModal = () => {
 
   return (
     <Modal isOpen={walletModalOpen} onDismiss={toggleWalletModal}>
-      <div className="mb-8">
-        <p className="text-center text-xl mb-3">Connect your wallet</p>
-        <div className="w-full h-px h-1 bg-primary-500" />
-        <p className="text-center leading-tight mt-6">
-          As a final step your wallet will ask you to authorize Union to access
-          your account.
+      <div className="px-4 py-6 sm:px-6 sm:py-8">
+        <div className="mb-8">
+          <p className="text-center text-xl mb-3">Connect your wallet</p>
+          <div className="w-full h-px h-1 bg-primary-500" />
+          <p className="text-center leading-tight mt-6">
+            As a final step your wallet will ask you to authorize Union to
+            access your account.
+          </p>
+        </div>
+
+        <div className="mt-8 mb-10">
+          <div className="mt-4">
+            <Button full type="invert">
+              MetaMask
+            </Button>
+          </div>
+          <div className="mt-4">
+            <Button full type="invert">
+              WalletConnect
+            </Button>
+          </div>
+          <div className="mt-4">
+            <Button full type="invert">
+              Fortmatic
+            </Button>
+          </div>
+        </div>
+
+        <div className="w-full h-px bg-accent" />
+
+        <p className="text-sm text-center mt-6">
+          Don't have an account?{" "}
+          <button className="underline font-medium">Start now</button>
         </p>
       </div>
-
-      <div className="mt-8 mb-10">
-        <div className="mt-4">
-          <Button full type="invert">
-            MetaMask
-          </Button>
-        </div>
-        <div className="mt-4">
-          <Button full type="invert">
-            WalletConnect
-          </Button>
-        </div>
-        <div className="mt-4">
-          <Button full type="invert">
-            Fortmatic
-          </Button>
-        </div>
-      </div>
-
-      <div className="w-full h-px bg-accent" />
-
-      <p className="text-sm text-center mt-6">
-        Don't have an account?{" "}
-        <button className="underline font-medium">Start now</button>
-      </p>
     </Modal>
   );
 };
