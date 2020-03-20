ba4469488ba5a17da2950c2945446043f68bbfef
Jeff Reiner <theundeadghost@gmail.com>
2020-03-20 20:59:13 +0100

    :sparkles: Build input component

--- components/input.js
+++ components/input.js
@@ -1,21 +1,45 @@
 import { forwardRef } from "react";
 
-const Input = forwardRef(({ label, tip, ...props }, ref) => {
-  return (
-    <div>
-      <input
-        ref={ref}
-        type="text"
-        {...[...props, tip && { "aria-describedby": `inputTip` }]}
-      />
+const Input = forwardRef(
+  ({ className, id, label, chip, tip, ...props }, ref) => {
+    return (
+      <div className={className}>
+        <div className="p-4 w-full rounded border bg-white">
+          {label && (
+            <label className="block leading-none mb-2 text-sm" htmlFor={id}>
+              {label}
+            </label>
+          )}
 
-      {tip && (
-        <div>
-          <span id={`inputTip`}>{tip}</span>
+          <div className="flex">
+            <input
+              id={id}
+              name={id}
+              ref={ref}
+              type="text"
+              className="focus:outline-none focus:shadow-outline text-lg leading-snug flex-1"
+              autoComplete="off"
+              {...props}
+              {...(tip && { "aria-describedby": `${id}InputTip` })}
+            />
+
+            {chip && (
+              <span className="font-semibold ml-4 select-none">DAI</span>
+            )}
+          </div>
         </div>
-      )}
-    </div>
-  );
-});
+
+        {tip && (
+          <span
+            className="block text-xs leading-tight mt-2"
+            id={`${id}InputTip`}
+          >
+            {tip}
+          </span>
+        )}
+      </div>
+    );
+  }
+);
 
 export default Input;
