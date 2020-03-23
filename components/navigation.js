dd61a22a90b8986ce13f12725e4218bd028f6c1a
Jeff Reiner <theundeadghost@gmail.com>
2020-03-23 22:39:20 +0100

    :sparkles: Working on the nav

--- components/navigation.js
+++ components/navigation.js
@@ -4,42 +4,42 @@ import Web3Status from "./web3Connection";
 
 const Navigation = () => {
   return (
-    <nav className="border-b mb-10 py-4">
+    <nav className="border-b mb-10">
       <div className="w-full mx-auto max-w-screen-xl">
         <ul className="flex items-center justify-between">
-          <li>
+          <li className="py-4">
             <Link href="/">
-              <a>
+              <a className="inline-flex items-center h-12">
                 <Logo />
               </a>
             </Link>
           </li>
 
-          <ul className="flex items-center">
+          <ul className="flex items-center py-4 h-20">
             <li>
               <Link href="/stake">
-                <a className="p-3 leading-none mx-4 text-lg font-semibold">
+                <a className="p-3 leading-none mx-4 text-lg font-semibold active-nav-tab">
                   Stake
                 </a>
               </Link>
             </li>
             <li>
               <Link href="/borrow">
-                <a className="p-3 leading-none mx-4 text-lg font-semibold">
+                <a className="p-3 leading-none mx-4 text-lg font-semibold active-nav-tab">
                   Borrow
                 </a>
               </Link>
             </li>
             <li>
               <Link href="/vouch">
-                <a className="p-3 leading-none mx-4 text-lg font-semibold">
+                <a className="p-3 leading-none mx-4 text-lg font-semibold active-nav-tab">
                   Vouch
                 </a>
               </Link>
             </li>
           </ul>
 
-          <ul className="flex items-center">
+          <ul className="flex items-center py-4">
             <li className="mr-8">
               <span
                 className="leading-none text-2xl"
