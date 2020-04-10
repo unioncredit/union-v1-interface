a43e521e6fc936a360de050bc1e59bdcf0e1a5f5
Jeff Reiner <theundeadghost@gmail.com>
2020-04-10 15:59:54 +0200

    :art: Moving preload font bits to _document

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Footer from "@components/footer";
 import Navigation from "@components/navigation";
 import ApplicationContext from "@contexts/Application";
 import BorrowContext from "@contexts/Borrow";
@@ -5,9 +6,7 @@ import StakeContext from "@contexts/Stake";
 import VouchContext from "@contexts/Vouch";
 import getLibrary from "@lib/getLibrary";
 import { Web3ReactProvider } from "@web3-react/core";
-import Head from "next/head";
 import "../css/tailwind.css";
-import Footer from "@components/footer";
 
 const ContextProviders = ({ children }) => (
   <ApplicationContext>
@@ -22,30 +21,6 @@ const ContextProviders = ({ children }) => (
 export default function UnionApp({ Component, pageProps }) {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
-      <Head>
-        <link
-          rel="preload"
-          href="/Montserrat-400.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/Montserrat-500.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/Montserrat-600.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-      </Head>
-
       <ContextProviders>
         <header>
           <Navigation />
--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,27 @@ class MyDocument extends Document {
       <Html lang="en" className="font-medium antialiased text-type-base">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="preload"
+            href="/Montserrat-400.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/Montserrat-500.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/Montserrat-600.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
         </Head>
         <body>
           <Main />
