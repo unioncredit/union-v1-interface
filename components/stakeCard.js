7d95b368b102cc7abeea087281608875fd08e7fd
Jeff Reiner <theundeadghost@gmail.com>
2020-04-13 14:51:30 +0200

    :recycle: Moving StakeCard data fetching from stake to itself

--- components/stakeCard.js
+++ components/stakeCard.js
@@ -1,19 +1,105 @@
+import { TOKENS } from "@constants/index";
 import { useDepositModalToggle, useWithdrawModalToggle } from "@contexts/Stake";
+import { getRewards } from "@lib/contracts/getRewards";
+import { getRewardsMultiplier } from "@lib/contracts/getRewardsMultiplier";
+import { getStakeAmount } from "@lib/contracts/getStakeAmount";
+import { getSupplyPerYear } from "@lib/contracts/getSupplyPerYear";
+import { stake } from "@lib/contracts/stake";
+import { unstake } from "@lib/contracts/unstake";
+import { useWeb3React } from "@web3-react/core";
+import { useEffect, useState } from "react";
 import { placeholderTip } from "../text/tooltips";
 import Button from "./button";
 import DepositModal from "./depositModal";
 import LabelPair from "./labelPair";
 import WithdrawModal from "./withdrawModal";
 
-const StakeCard = ({ data, onDeposit, onWithdraw }) => {
+const StakeCard = () => {
   const toggleDepositModal = useDepositModalToggle();
   const toggleWithdrawModal = useWithdrawModalToggle();
+  const { account, library, chainId } = useWeb3React();
+
+  const [totalStake, setTotalStake] = useState("N/A");
+  const [utilizedStake, setUtilizedStake] = useState("N/A");
+  const [defaultedStake, setDefaultedStake] = useState("N/A");
+  const [withdrawableStake, setWithdrawableStake] = useState("N/A");
+  const [rewards, setRewards] = useState("N/A");
+  const [upy, setUpy] = useState("N/A");
+  const [rewardsMultiplier, setRewardsMultiplier] = useState(0);
+  const [signer, setSigner] = useState(undefined);
+
+  useEffect(() => {
+    if (library && account) {
+      getStakeData();
+      getRewardData();
+      getUpyData();
+      getRewardsMultiplierData();
+      setSigner(library.getSigner());
+    }
+  }, [library, account]);
+
+  const getStakeData = async () => {
+    const res = await getStakeAmount(
+      account,
+      TOKENS[chainId]["DAI"],
+      library,
+      chainId
+    );
+    setTotalStake(res.stakingAmount);
+    setUtilizedStake(res.lendingAmount);
+    setDefaultedStake(res.freezeAmount);
+    setWithdrawableStake(res.stakingAmount.sub(res.vouchingAmount));
+  };
+
+  const getRewardData = async () => {
+    const res = await getRewards(TOKENS[chainId]["DAI"], library, chainId);
+    setRewards(res);
+  };
+
+  const getUpyData = async () => {
+    const res = await getSupplyPerYear(
+      TOKENS[chainId]["DAI"],
+      library,
+      chainId
+    );
+    setUpy(res);
+  };
+
+  const getRewardsMultiplierData = async () => {
+    const res = await getRewardsMultiplier(
+      TOKENS[chainId]["DAI"],
+      library,
+      chainId
+    );
+    setRewardsMultiplier(res);
+  };
+
+  const onDeposit = async (amount) => {
+    await stake(TOKENS[chainId]["DAI"], amount, signer, chainId);
+  };
+
+  const onWithdraw = async (amount) => {
+    await unstake(TOKENS[chainId]["DAI"], amount, signer, chainId);
+  };
+
+  /**
+   * @todo move format to LabelPair component as a prop
+   */
+  const stakeCardData = {
+    totalStake: `${totalStake} DAI`,
+    utilizedStake: `${utilizedStake} DAI`,
+    defaultedStake: `${defaultedStake} DAI`,
+    withdrawableStake: `${withdrawableStake} DAI`,
+    rewardsMultiplier: `${rewardsMultiplier}x`,
+    rewards: `${rewards} UNION`,
+    upy: `${upy} UNION`,
+  };
 
   return (
     <div className="bg-pink-light border border-pink-pure rounded p-4 md:p-6">
       <LabelPair
         label="Your total stake"
-        value={data.totalStake}
+        value={totalStake}
         className="mb-4"
         large
       />
@@ -22,24 +108,24 @@ const StakeCard = ({ data, onDeposit, onWithdraw }) => {
         className="text-grey-pure"
         label="Utilized Stake"
         tooltip={placeholderTip}
-        value={data.utilizedStake}
+        value={utilizedStake}
       />
       <LabelPair
         className="text-grey-pure"
         label="Defaulted Stake"
         tooltip={placeholderTip}
-        value={data.defaultedStake}
+        value={defaultedStake}
       />
       <LabelPair
         className="text-grey-pure"
         label="Withdrawable Stake"
         tooltip={placeholderTip}
-        value={data.withdrawableStake}
+        value={withdrawableStake}
       />
 
       <LabelPair
         label="Rewards multiplier"
-        value={data.rewardsMultiplier}
+        value={rewardsMultiplier}
         className="mb-4 mt-12"
         large
       />
@@ -48,13 +134,13 @@ const StakeCard = ({ data, onDeposit, onWithdraw }) => {
         className="text-grey-pure"
         label="Rewards"
         tooltip={placeholderTip}
-        value={data.rewards}
+        value={rewards}
       />
       <LabelPair
         className="text-grey-pure"
         label="UNION Per Year (est.)"
         tooltip={placeholderTip}
-        value={data.upy}
+        value={upy}
       />
       <div className="flex -mx-3 mt-10">
         <div className="flex-1 px-3">
@@ -69,8 +155,8 @@ const StakeCard = ({ data, onDeposit, onWithdraw }) => {
         </div>
       </div>
 
-      <DepositModal totalStake={data.totalStake} onDeposit={onDeposit} />
-      <WithdrawModal totalStake={data.totalStake} onWithdraw={onWithdraw} />
+      <DepositModal totalStake={totalStake} onDeposit={onDeposit} />
+      <WithdrawModal totalStake={totalStake} onWithdraw={onWithdraw} />
     </div>
   );
 };
--- pages/stake.js
+++ pages/stake.js
@@ -6,18 +6,12 @@ import TutorialModal from "@components/tutorialModal";
 import { TOKENS } from "@constants/index";
 import { useEmailModalToggle } from "@contexts/Application";
 import { useTrustModalToggle, useTutorialModalToggle } from "@contexts/Stake";
-import { getRewards } from "@lib/contracts/getRewards";
-import { getRewardsMultiplier } from "@lib/contracts/getRewardsMultiplier";
-import { getStakeAmount } from "@lib/contracts/getStakeAmount";
-import { getSupplyPerYear } from "@lib/contracts/getSupplyPerYear";
 import { getTrust } from "@lib/contracts/getTrust";
-import { stake } from "@lib/contracts/stake";
-import { unstake } from "@lib/contracts/unstake";
 import { vouch } from "@lib/contracts/vouch";
 import { useWeb3React } from "@web3-react/core";
 import Head from "next/head";
 import { parseCookies } from "nookies";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 export default function Stake() {
   const { account, library, chainId } = useWeb3React();
@@ -25,28 +19,9 @@ export default function Stake() {
   const toggleEmailModal = useEmailModalToggle();
   const toggleTutorialModal = useTutorialModalToggle();
 
-  const [totalStake, setTotalStake] = useState("N/A");
-  const [utilizedStake, setUtilizedStake] = useState("N/A");
-  const [defaultedStake, setDefaultedStake] = useState("N/A");
-  const [withdrawableStake, setWithdrawableStake] = useState("N/A");
-  const [rewards, setRewards] = useState("N/A");
-  const [upy, setUpy] = useState("N/A");
-  const [rewardsMultiplier, setRewardsMultiplier] = useState(0);
   const [trustData, setTrustData] = useState([]);
-  const [signer, setSigner] = useState(undefined);
 
   const { email_modal_completed, tutorial_modal_completed } = parseCookies();
-
-  useEffect(() => {
-    if (library && account) {
-      getStakeData();
-      getRewardData();
-      getUpyData();
-      getRewardsMultiplierData();
-      getTrustData();
-      setSigner(library.getSigner());
-    }
-  }, [library, account]);
 
   useEffect(() => {
     if (!email_modal_completed) toggleEmailModal();
@@ -57,73 +32,26 @@ export default function Stake() {
       toggleTutorialModal();
   }, [email_modal_completed, tutorial_modal_completed]);
 
-  const getStakeData = async () => {
-    const res = await getStakeAmount(
-      account,
-      TOKENS[chainId]["DAI"],
-      library,
-      chainId
-    );
-    setTotalStake(res.stakingAmount);
-    setUtilizedStake(res.lendingAmount);
-    setDefaultedStake(res.freezeAmount);
-    setWithdrawableStake(res.stakingAmount.sub(res.vouchingAmount));
-  };
+  useEffect(() => {
+    if (library && account) {
+      (async () => {
+        const res = await getTrust(
+          account,
+          TOKENS[chainId]["DAI"],
+          library,
+          chainId
+        );
 
-  const getRewardData = async () => {
-    const res = await getRewards(TOKENS[chainId]["DAI"], library, chainId);
-    setRewards(res);
-  };
-
-  const getUpyData = async () => {
-    const res = await getSupplyPerYear(
-      TOKENS[chainId]["DAI"],
-      library,
-      chainId
-    );
-    setUpy(res);
-  };
-
-  const getRewardsMultiplierData = async () => {
-    const res = await getRewardsMultiplier(
-      TOKENS[chainId]["DAI"],
-      library,
-      chainId
-    );
-    setRewardsMultiplier(res);
-  };
-
-  const getTrustData = async () => {
-    const res = await getTrust(
-      account,
-      TOKENS[chainId]["DAI"],
-      library,
-      chainId
-    );
-    setTrustData(res);
-  };
-
-  const onDeposit = async (amount) => {
-    await stake(TOKENS[chainId]["DAI"], amount, signer, chainId);
-  };
-
-  const onWithdraw = async (amount) => {
-    await unstake(TOKENS[chainId]["DAI"], amount, signer, chainId);
-  };
+        setTrustData(res);
+      })();
+    }
+  }, [library, account, chainId]);
 
   const onTrust = async (address, amount) => {
     await vouch(address, TOKENS[chainId]["DAI"], amount, signer, chainId);
   };
 
-  const stakeCardData = {
-    totalStake: `${totalStake} DAI`,
-    utilizedStake: `${utilizedStake} DAI`,
-    defaultedStake: `${defaultedStake} DAI`,
-    withdrawableStake: `${withdrawableStake} DAI`,
-    rewardsMultiplier: `${rewardsMultiplier}x`,
-    rewards: `${rewards} UNION`,
-    upy: `${upy} UNION`,
-  };
+  const stakeTableData = useMemo(() => trustData, [trustData]);
 
   return (
     <div>
@@ -150,15 +78,11 @@ export default function Stake() {
 
         <div className="flex -mx-2">
           <div className="w-5/12 px-2">
-            <StakeCard
-              data={stakeCardData}
-              onDeposit={onDeposit}
-              onWithdraw={onWithdraw}
-            />
+            <StakeCard />
           </div>
 
           <div className="w-7/12 px-2">
-            <StakeTable data={trustData} />
+            <StakeTable data={stakeTableData} />
           </div>
         </div>
       </div>
