be258989ad2ac8439b4841c2d2398fe086a9fef4
Jeff Reiner <theundeadghost@gmail.com>
2020-04-13 16:54:58 +0200

    :art: Fix missing padding for Container and Nav

--- components/footer.js
+++ components/footer.js
@@ -30,16 +30,18 @@ const Footer = () => {
     <footer className="border-t border-border-pure py-6 mt-8">
       <div className="container">
         <div className="flex">
-          <ul className="flex w-3/4 flex-wrap text-type-lightest text-sm -mr-4 -mb-2">
-            {FOOTER_LINKS.map(({ href, label }, i) => (
-              <li key={i} className="pb-2 pr-4">
-                <a className="hover:underline" href={href}>
-                  {label}
-                </a>
-              </li>
-            ))}
-            <li className="pb-2 pr-4">© 2020 Union.Finance</li>
-          </ul>
+          <div className="w-3/4">
+            <ul className="flex flex-wrap text-type-lightest text-sm -mr-4 -mb-2 flex-wrap">
+              {FOOTER_LINKS.map(({ href, label }, i) => (
+                <li key={i} className="pb-2 pr-4">
+                  <a className="hover:underline" href={href}>
+                    {label}
+                  </a>
+                </li>
+              ))}
+              <li className="pb-2 pr-4">© 2020 Union.Finance</li>
+            </ul>
+          </div>
           <ul className="flex w-1/4 justify-end">
             <li className="ml-4">
               <div className="h-6 w-6 rounded-full bg-type-lightest"></div>
