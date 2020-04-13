d27de72fc43f90cd5afb984c873c7f3355e97d73
Jeff Reiner <theundeadghost@gmail.com>
2020-04-13 23:12:55 +0200

    :lipstick: Adding underline to Contact support link

--- pages/_error.js
+++ pages/_error.js
@@ -5,7 +5,10 @@ export default function Error({ statusCode }) {
         <h1>Something went wrong</h1>
         <p className="text-lg mt-4">
           An unexpected error occurred.{" "}
-          <a href="mailto:support@union.finance">Contact Support</a>.
+          <a className="underline" href="mailto:support@union.finance">
+            Contact Support
+          </a>
+          .
         </p>
       </div>
     </div>
