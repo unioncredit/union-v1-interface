109b6de24ab73a9ea19fd701cd2bafbab04d83ff
twygod <twygod@gmail.com>
2020-04-14 20:35:29 +0800

    Kovan smart contract address add

--- constants/index.js
+++ constants/index.js
@@ -13,6 +13,7 @@ export const MARKET_REGISTRY_ADDRESSES = {
   3: AddressZero,
   4: AddressZero,
   5: AddressZero,
+  42: "0x5A41427313C07625e98d68cAb9571C277aBc889B",
   1337: "0x2E8Ef1D7648ea92c2d8EaE63EaE063d9370CEE0F",
 };
 
@@ -21,6 +22,7 @@ export const MEMBER_MAMAGER_ADDRESSES = {
   3: AddressZero,
   4: AddressZero,
   5: AddressZero,
+  42: "0x888B7b6Cb012e92EC8fFb72e2Fe320d0A6002fD7",
   1337: "0x4216a9b087626cFB8b6d047215F4BF7924B48086",
 };
 
@@ -29,12 +31,16 @@ export const STAKING_MAMAGER_ADDRESSES = {
   3: AddressZero,
   4: AddressZero,
   5: AddressZero,
+  42: "0xAf2B73080aC6542a77b8A98dACaA93ab680F48ea",
   1337: "0x97cEd657FCD084FDa141f3ad078B32820BcE26a6",
 };
 
 export const SCALE = 10 ** 18;
 
 export const TOKENS = {
+  42: {
+    DAI: "0x986671E78cF5991e248E17aa02948F1f22E9B0Df",
+  },
   1337: {
     DAI: "0x442ab2cA11e8ECACfB0cAc9fEd41725b4FC72B27",
   },
