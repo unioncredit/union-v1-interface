3fe8394a47381358db140b398576fa7bba915f06
Jeff Reiner <theundeadghost@gmail.com>
2020-04-15 12:51:42 +0200

    Hooking up modal

--- components/creditRequestModal.js
+++ components/creditRequestModal.js
@@ -7,16 +7,17 @@ import { useForm } from "react-hook-form";
 import Button from "./button";
 import Input from "./input";
 import Modal, { ModalHeader } from "./modal";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import dyanmic from "next/dynamic";
+import { useCopyToClipboard } from "react-use";
 
 const QRCode = dyanmic(() => import("./shareQRCode"));
 
 const generateLink = (address, amount = 0) => {
   if (!address) throw new Error("`account` is required");
 
-  return `${window.location}?address=${address}&trust=${amount}`;
+  return `${window.location.origin}/stake?address=${address}&trust=${amount}`;
 };
 
 const CreditRequestModal = () => {
@@ -24,48 +25,33 @@ const CreditRequestModal = () => {
   const toggle = useCreditRequestModalToggle();
 
   const [shareLink, setShareLink] = useState(undefined);
+  const [{ value: copied }, copyToClipboard] = useCopyToClipboard();
 
   const { account } = useWeb3React();
 
   const { handleSubmit, register } = useForm();
 
-  const onSubmit = (values) => {
+  const onSubmit = async (values) => {
+    const { amount } = values;
+
     try {
-      console.log(generateLink(account, values.amount));
+      setShareLink(generateLink(account, amount));
     } catch (err) {
       console.error(err);
     }
   };
 
+  useEffect(() => {
+    return () => {
+      setShareLink(undefined);
+    };
+  }, [open]);
+
   return (
     <Modal isOpen={open} onDismiss={toggle}>
       <ModalHeader title="Ask for a vouch" onDismiss={toggle} />
 
-      <form
-        method="POST"
-        onSubmit={handleSubmit(onSubmit)}
-        className="px-4 py-6 sm:px-6 sm:py-8"
-      >
-        <p className="mb-4">Ask someone to vouch for you</p>
-        <Input
-          chip="DAI"
-          id="amount"
-          label="Amount"
-          name="amount"
-          placeholder="0.00"
-          ref={register}
-          tip="Tip: You can leave this amount input open"
-          type="number"
-        />
-
-        <div className="mt-20">
-          <Button full type="submit">
-            Generate link
-          </Button>
-        </div>
-      </form>
-
-      {shareLink && (
+      {shareLink ? (
         <div className="text-center px-4 py-6 sm:px-6 sm:py-8">
           <div className="flex justify-center mb-8">
             <QRCode link={shareLink} />
@@ -76,8 +62,34 @@ const CreditRequestModal = () => {
             Union and have them vouch for you.
           </p>
           <Input value={shareLink} readonly className="mb-4" />
-          <Button full>Copy link</Button>
+          <Button full onClick={() => copyToClipboard(shareLink)}>
+            {copied ? "Copied!" : "Copy link"}
+          </Button>
         </div>
+      ) : (
+        <form
+          method="POST"
+          onSubmit={handleSubmit(onSubmit)}
+          className="px-4 py-6 sm:px-6 sm:py-8"
+        >
+          <p className="mb-4">Ask someone to vouch for you</p>
+          <Input
+            chip="DAI"
+            id="amount"
+            label="Amount"
+            name="amount"
+            placeholder="0.00"
+            ref={register}
+            tip="Tip: You can leave this amount input open"
+            type="number"
+          />
+
+          <div className="mt-20">
+            <Button full type="submit">
+              Generate link
+            </Button>
+          </div>
+        </form>
       )}
     </Modal>
   );
