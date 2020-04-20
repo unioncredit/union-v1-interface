2d925edcfaa494e342a8d94dbf523bed25ba0210
Jeff Reiner <theundeadghost@gmail.com>
2020-04-20 15:58:24 +0200

    Update snippet code

--- components/snippet.js
+++ components/snippet.js
@@ -1,5 +1,5 @@
 const Snippet = () => (
-  <div className="relative flex justify-center lg:justify-start pt-8 md:pt-0">
+  <div className="relative select-none flex justify-center lg:justify-start pt-8 md:pt-0">
     <img
       className="w-16 lg:w-32 absolute -mt-8 lg:-ml-20 lg:-mt-12"
       src="/images/snippet.svg"
@@ -10,23 +10,40 @@ const Snippet = () => (
         <pre role="presentation">
           <span role="presentation">
             <span className="pl-k">contract</span>{" "}
-            <span className="pl-c1">CToken</span>{" "}
-            <span className="pl-k">is</span> IERC20 {"{"}
+            <span className="pl-c1">LendingMarket</span>{" "}
+            <span className="text-type-light">...</span> {"{"}
           </span>
         </pre>
         <pre role="presentation">
           <span role="presentation">
             &nbsp;&nbsp;
             <span className="pl-k">function</span>{" "}
-            <span className="pl-en">supplyRatePerBlock</span>
-            ()
+            <span className="pl-en">borrow</span>(
+            <span className="pl-c1">uint256</span> <span>amount</span>)
           </span>
         </pre>
         <pre role="presentation">
           <span role="presentation">
             &nbsp;&nbsp;&nbsp;&nbsp;
-            <span className="pl-k">external view returns</span> (
-            <span className="pl-c1">uint256</span>);
+            <span className="pl-k">external</span>
+          </span>
+        </pre>
+        <pre role="presentation">
+          <span role="presentation">
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <span className="pl-k">onlyMember</span>(
+            <span className="pl-k">msg</span>.
+            <span className="pl-k">sender</span>
+            <span>)</span>
+          </span>
+        </pre>
+        <pre role="presentation">
+          <span role="presentation">
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <span className="pl-k">whenNotPaused</span>(
+            <span className="pl-k">address</span>(
+            <span className="pl-k">poolToken</span>
+            ))
           </span>
         </pre>
         <pre role="presentation">
@@ -36,37 +53,32 @@ const Snippet = () => (
           <span role="presentation">
             &nbsp;&nbsp;
             <span className="pl-k">function</span>{" "}
-            <span className="pl-en">mint</span>(
-            <span className="pl-c1">uint256</span> <span>mintAmount</span>)
+            <span className="pl-en">repay</span>(
+            <span className="pl-c1">uint256</span> <span>amount</span>)
           </span>
         </pre>
         <pre role="presentation">
           <span role="presentation">
             &nbsp;&nbsp;&nbsp;&nbsp;
-            <span className="pl-k">external returns</span> (
-            <span className="pl-c1">uint256</span>);
-          </span>
-        </pre>
-        <pre role="presentation">
-          <span role="presentation">&nbsp;</span>
-        </pre>
-        <pre role="presentation">
-          <span role="presentation">
-            &nbsp;&nbsp;
-            <span className="pl-k">function</span>{" "}
-            <span className="pl-en">redeemUnderlying</span>(
+            <span className="pl-k">external</span>
           </span>
         </pre>
         <pre role="presentation">
           <span role="presentation">
             &nbsp;&nbsp;&nbsp;&nbsp;
-            <span className="pl-c1">uint256</span> <span>redeemAmount</span>
+            <span className="pl-k">onlyMember</span>(
+            <span className="pl-k">msg</span>.
+            <span className="pl-k">sender</span>
+            <span>)</span>
           </span>
         </pre>
         <pre role="presentation">
           <span role="presentation">
-            &nbsp;&nbsp;) <span className="pl-k">external returns</span> (
-            <span className="pl-c1">uint256</span>);
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <span className="pl-k">whenNotPaused</span>(
+            <span className="pl-k">address</span>(
+            <span className="pl-k">poolToken</span>
+            ))
           </span>
         </pre>
         <pre role="presentation">
