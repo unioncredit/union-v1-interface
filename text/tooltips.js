3094a4294645ba3ae92cad38994516d9446d3b6f
Jeff Reiner <theundeadghost@gmail.com>
2020-04-23 23:14:52 +0200

    Fix items

--- text/tooltips.js
+++ text/tooltips.js
@@ -1,6 +1,3 @@
-export const placeholderTip =
-  "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
-
 export const utilizedStakeTip =
   "Utilized Stake.\n\nThis is the stake that is currently being used by someone. It cannot be withdrawn.";
 
@@ -8,7 +5,7 @@ export const defaultedStakeTip =
   "Defaulted Stake.\n\nThis part shows the total amount of people which have not paid on time. Funds that are locked in defaulted stake are not earning UPY";
 
 export const withdrawableStakeTip =
-  "Withdrawable Stake.\n\nThis is the stake you can actually withdraw. It is “Your total stake” minus “Your utilized stake”";
+  'Withdrawable Stake.\n\nThis is the stake you can actually withdraw. It is "Your total stake” minus “Your utilized stake"';
 
 export const vouchingTip =
   "Vouching.\n\nVouching for someone is similiar to cosigning, you are signalling to Union that they can be trusted to borrow and repay. Only UNION members are able to vouch.";
@@ -26,4 +23,4 @@ export const minimumPaymentDueTip =
   "Minimum Payment Due.\n\nThis is the amount you need to repay monthly";
 
 export const unionPerYearTip =
-  "UNION Per Year.\n\nThis is the token that the staking contract returns to the user that is based on the DAI staked and reward multiplier algorithm. Each block inflates based on the total amount of DAI staked.\n\nYour share of the inflation is:\n\nYour not defaulted DAI stake x your reward multiplier / Sum of everyone elses above number";
+  'UNION Per Year.\n\nThis is the token that the staking contract returns to the user that is based on the DAI staked and reward multiplier algorithm. Each block inflates based on the total amount of DAI staked.\n\nYour share of the inflation is:\n\n"Your not defaulted DAI stake" times "your reward multiplier" divided by "Sum of everyone elses above number"';
