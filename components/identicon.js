e6e6e5afd61da483b13f0c9ddfd7afbcf4d7e305
Jeff Reiner <theundeadghost@gmail.com>
2020-05-05 17:16:39 +0200

    Update colors for Identicon

--- components/identicon.js
+++ components/identicon.js
@@ -2,22 +2,21 @@ import { memo } from "react";
 import Jazzicon, { jsNumberForAddress } from "react-jazzicon-custom-colors";
 
 const jazziconTheme = [
-  "#01888c",
-  "#fc7500",
-  "#034f5d",
-  "#f73f01",
-  "#fc1960",
-  "#c7144c",
-  "#f3c100",
-  "#1598f2",
-  "#2465e1",
-  "#f19e02",
+  "#418CFC",
+  "#FDAFA6",
+  "#A6B9FD",
+  "#F4DBD8",
+  "#A6D3FD",
+  "#2900A0",
+  "#E8F1E9",
+  "#F9C4BE",
+  "#48666A",
 ];
 
 const Identicon = memo(({ address, extraLarge = false, large = false }) => {
   return (
     <Jazzicon
-      color={jazziconTheme}
+      colors={jazziconTheme}
       diameter={extraLarge ? 72 : large ? 32 : 18}
       seed={jsNumberForAddress(address)}
     />
