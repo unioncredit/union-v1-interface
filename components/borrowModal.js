4f4e6e3ac68982b9b9377507ac70afc7b7012e99
twygod <twygod@gmail.com>
2020-05-11 18:10:27 +0800

    Fix the display of credit limit in the Borrow box

--- components/borrowModal.js
+++ components/borrowModal.js
@@ -42,14 +42,13 @@ const BorrowModal = ({
   const formatNewCredit = Number(
     parseFloat(creditLimit) > 0
       ? parseFloat(creditLimit) -
-          parseFloat(balanceOwed) -
-          parseFloat(amount || 0)
+      parseFloat(amount || 0)
       : 0
   ).toFixed(2);
 
   const maxBorrowable = Number(
     parseFloat(creditLimit) > 0
-      ? (parseFloat(creditLimit) - parseFloat(balanceOwed)) * (1 - fee)
+      ? parseFloat(creditLimit)
       : 0
   ).toFixed(2);
 
