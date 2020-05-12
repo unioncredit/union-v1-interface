9680f049ba32115b3e587c77b628ee12f3d64f9d
twygod <twygod@gmail.com>
2020-05-12 14:12:38 +0800

    Adjust trust number type error repair

--- components/adjustTrustForm.js
+++ components/adjustTrustForm.js
@@ -25,7 +25,7 @@ const AdjustTrustForm = ({ address, vouched, onComplete }) => {
   const amount = watch("amount", 0);
 
   const formatNewTrust = Number(
-    vouched + ((adjustType === ADJUST_TYPES.INCREASE ? 1 : -1) * amount || 0)
+    parseFloat(vouched) + ((adjustType === ADJUST_TYPES.INCREASE ? 1 : -1) * amount || 0)
   );
 
   const onSubmit = async (data, e) => {
