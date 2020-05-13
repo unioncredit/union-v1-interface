7a6da9b08562681f78d42c9f92a4ee02736088c7
kingjacob <kingjacob@gmail.com>
2020-05-13 02:26:25 -0500

    Revert "Enhancement: Add Heap analytics"
    This reverts commit dc002a53b7990806da5b6375fb86de8305004056.

--- pages/_document.js
+++ pages/_document.js
@@ -45,12 +45,6 @@ class MyDocument extends Document {
           <meta property="og:description" content={META_TAGS.DESCRIPTION} />
           <meta name="twitter:description" content={META_TAGS.DESCRIPTION} />
           <meta name="theme-color" content="#032437" />
-      <%- if environment? :production %>
-      <script type="text/javascript">
-      window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
-    heap.load("3604841211");
-    </script>
-    <%- end %>
         </Head>
         <body>
           <Main />
