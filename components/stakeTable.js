5effee9d4cd9c0b39bfd0e374978173074048c03
Jeff Reiner <theundeadghost@gmail.com>
2020-06-02 21:25:58 +0200

    Remove expand from StakeTable

--- components/stakeTable.js
+++ components/stakeTable.js
@@ -1,10 +1,9 @@
 import { useLearnMoreModalToggle } from "contexts/Application";
 import { useAddressModalToggle, useTrustModalToggle } from "contexts/Stake";
-import { useAutoCallback } from "hooks.macro";
 import useIsMember from "hooks/useIsMember";
 import useTrustData from "hooks/useTrustData";
 import { Fragment, useMemo, useState } from "react";
-import { useExpanded, useSortBy, useTable } from "react-table";
+import { useSortBy, useTable } from "react-table";
 import Chevron from "svgs/Chevron";
 import Info from "svgs/Info";
 import { healthTip } from "text/tooltips";
@@ -67,17 +66,7 @@ const renderSortIcons = (column) => (
  * @param {import("react-table").ColumnInstance} column
  */
 const renderTheadColumns = (column) => {
-  const { Header, id } = column;
-
-  if (id === "expander")
-    return (
-      <th
-        className="sm:hidden"
-        {...column.getHeaderProps(column.getSortByToggleProps())}
-      >
-        {column.render("Header")}
-      </th>
-    );
+  const { Header } = column;
 
   if (Header === "Health")
     return (
@@ -134,13 +123,6 @@ const renderTbodyCells = (cell) => {
       </td>
     );
 
-  if (cell.column.id === "expander")
-    return (
-      <td className="w-8 sm:hidden" {...cell.getCellProps()}>
-        {cell.render("Cell")}
-      </td>
-    );
-
   return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
 };
 
@@ -162,22 +144,6 @@ const StakeTable = () => {
 
   const memoizedColumns = useMemo(
     () => [
-      {
-        Header: () => null,
-        id: "expander",
-        Cell: ({ row }) => (
-          <button
-            className="h-6 w-6 ml-0 my-1 mr-3 flex items-center justify-center focus:outline-none focus:shadow-outline rounded"
-            {...row.getToggleRowExpandedProps()}
-          >
-            {row.isExpanded ? (
-              <Chevron.Down size={16} />
-            ) : (
-              <Chevron.Right size={16} />
-            )}
-          </button>
-        ),
-      },
       {
         Header: "Address",
         accessor: "address",
@@ -205,7 +171,6 @@ const StakeTable = () => {
 
   const memoizedSortBy = useMemo(
     () => [
-      { id: "expander", desc: true },
       { id: "address", desc: true },
       { id: "trust", desc: true },
       { id: "used", desc: true },
@@ -220,7 +185,6 @@ const StakeTable = () => {
     headerGroups,
     rows,
     prepareRow,
-    visibleColumns,
   } = useTable(
     {
       columns: memoizedColumns,
@@ -230,28 +194,8 @@ const StakeTable = () => {
       },
       disableSortRemove: true,
     },
-    useSortBy,
-    useExpanded
+    useSortBy
   );
-
-  const renderRowSubComponent = useAutoCallback((row) => {
-    const { vouched, used } = row.values;
-
-    return (
-      <div className="bg-border-light text-type-light -m-4 px-4 py-3">
-        <ul className="ml-10">
-          <li className="h-5 leading-tight flex justify-between items-center mb-3">
-            <p className="text-sm">Vouched</p>
-            <p>{Number(vouched).toFixed()} DAI</p>
-          </li>
-          <li className="h-5 leading-tight flex justify-between items-center">
-            <p className="text-sm">Used</p>
-            <p>{Number(used).toFixed()} DAI</p>
-          </li>
-        </ul>
-      </div>
-    );
-  });
 
   return (
     <div className="sm:bg-white sm:border sm:rounded sm:p-6 h-full">
@@ -267,22 +211,14 @@ const StakeTable = () => {
           {rows.map((row) => {
             prepareRow(row);
             return (
-              <Fragment {...row.getRowProps()}>
-                <tr
-                  className="focus:outline-none cursor-pointer"
-                  onClick={handleRowClick(row)}
-                  tabIndex={0}
-                >
-                  {row.cells.map(renderTbodyCells)}
-                </tr>
-                {row.isExpanded ? (
-                  <tr>
-                    <td colSpan={visibleColumns.length}>
-                      {renderRowSubComponent(row)}
-                    </td>
-                  </tr>
-                ) : null}
-              </Fragment>
+              <tr
+                {...row.getRowProps()}
+                className="focus:outline-none cursor-pointer"
+                onClick={handleRowClick(row)}
+                tabIndex={0}
+              >
+                {row.cells.map(renderTbodyCells)}
+              </tr>
             );
           })}
         </tbody>
