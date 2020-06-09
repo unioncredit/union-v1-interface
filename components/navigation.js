6454697229d164c1daa8a6df925255d5f1ceea7e
Jeff Reiner <theundeadghost@gmail.com>
2020-06-09 13:57:59 +0000

    Showing nav on homepage if logged in

--- components/navigation.js
+++ components/navigation.js
@@ -61,79 +61,62 @@ const Navigation = () => {
   return (
     <nav className="border-b bg-white">
       <div className="w-full mx-auto px-4 max-w-screen-xl-gutter">
-        {isHomepage ? (
-          <ul className="flex items-center justify-between">
-            <LogoLink />
+        {/* Desktop */}
+        <ul className="flex items-center justify-between">
+          <LogoLink />
 
-            <li className="py-4">
-              <ul className="flex justify-end items-center">
-                {!!(account && library) ? (
+          {!!(account && library) ? (
+            <Fragment>
+              <li className="py-4 h-20 hidden md:flex flex-1 justify-center">
+                <ul className="flex justify-center items-center">
                   <li>
-                    <Web3Status />
+                    <NavigationLink href="/stake">Stake</NavigationLink>
                   </li>
-                ) : (
-                  <Fragment>
-                    <li className="mr-4">
-                      <button
-                        className="btn border-transparent font-medium px-4"
-                        onClick={toggleSignInModal}
-                      >
-                        Sign in
-                      </button>
-                    </li>
-                    <li>
-                      <Button secondary onClick={toggleCreateModal}>
-                        Start now
-                      </Button>
-                    </li>
-                  </Fragment>
-                )}
-              </ul>
+                  <li>
+                    <NavigationLink href="/borrow">Borrow</NavigationLink>
+                  </li>
+                  <li>
+                    <NavigationLink href="/vouch">Vouch</NavigationLink>
+                  </li>
+                </ul>
+              </li>
+
+              <li className="flex md:w-1/4 justify-end py-4">
+                <Web3Status />
+              </li>
+            </Fragment>
+          ) : (
+            <Fragment>
+              <li className="ml-auto mr-4">
+                <button
+                  className="btn border-transparent font-medium px-4"
+                  onClick={toggleSignInModal}
+                >
+                  Sign in
+                </button>
+              </li>
+              <li>
+                <Button secondary onClick={toggleCreateModal}>
+                  Start now
+                </Button>
+              </li>
+            </Fragment>
+          )}
+        </ul>
+
+        {/* Mobile */}
+        {!!(account && library) && (
+          <ul className="flex md:hidden justify-between sm:justify-evenly items-center pt-4 pb-6">
+            <li>
+              <NavigationLink href="/stake">Stake</NavigationLink>
+            </li>
+            <li>
+              <NavigationLink href="/borrow">Borrow</NavigationLink>
+            </li>
+            <li>
+              <NavigationLink href="/vouch">Vouch</NavigationLink>
             </li>
           </ul>
-        ) : (
-          <Fragment>
-            {/* Desktop */}
-            <ul className="flex items-center justify-between">
-              <LogoLink />
-
-              {!!(account && library) && (
-                <Fragment>
-                  <li className="py-4 h-20 hidden md:flex flex-1 justify-center">
-                    <ul className="flex justify-center items-center">
-                      <li>
-                        <NavigationLink href="/stake">Stake</NavigationLink>
-                      </li>
-                      <li>
-                        <NavigationLink href="/borrow">Borrow</NavigationLink>
-                      </li>
-                      <li>
-                        <NavigationLink href="/vouch">Vouch</NavigationLink>
-                      </li>
-                    </ul>
-                  </li>
-
-                  <li className="flex md:w-1/4 justify-end py-4">
-                    <Web3Status />
-                  </li>
-                </Fragment>
-              )}
-            </ul>
-
-            {!!(account && library) && (
-              <ul className="flex md:hidden justify-between sm:justify-evenly items-center pt-4 pb-6">
-                <li>
-                  <NavigationLink href="/stake">Stake</NavigationLink>
-                </li>
-                <li>
-                  <NavigationLink href="/borrow">Borrow</NavigationLink>
-                </li>
-                <li>
-                  <NavigationLink href="/vouch">Vouch</NavigationLink>
-                </li>
-              </ul>
-            )}
-          </Fragment>
         )}
       </div>
 
