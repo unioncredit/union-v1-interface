f98246292d7e511355888ce3a1d8ff6ed1f9559b
Jeff Reiner <theundeadghost@gmail.com>
2020-06-15 15:41:11 +0000

    Remove IPAPI

--- hooks/useIsSanctioned.js
+++ hooks/useIsSanctioned.js
@@ -2,14 +2,21 @@ import { OFAC_SANCTIONED } from "../constants/variables";
 import useSWR from "swr";
 import { useMemo } from "react";
 
-const API_KEY = process.env.NEXT_PUBLIC_IPAPI_KEY;
-
-const ENDPOINT = `https://api.ipapi.com/check?access_key=${API_KEY}&fields=country_code`;
+const ENDPOINT = "https://www.cloudflare.com/cdn-cgi/trace";
 
 const fetcher = async (...args) => {
   const res = await fetch(...args);
-  return res.json();
+  return res.text();
 };
+
+const keyValueToJSON = (raw) =>
+  raw.split("\n").reduce((acc, cur) => {
+    const [key, value] = cur.split("=");
+
+    acc[key] = value;
+
+    return acc;
+  }, {});
 
 /*
  * @name useIsSanctioned
@@ -17,19 +24,16 @@ const fetcher = async (...args) => {
  *   user's IP is located in an OFAC sanctioned country.
  */
 export default function useIsSanctioned() {
-  const { data, error } = useSWR(ENDPOINT, fetcher);
-
-  if (!!error) {
-    console.log("Error");
-    console.log(error);
-  }
-
-  console.log(JSON.stringify(data, null, 2));
+  const { data } = useSWR(ENDPOINT, fetcher);
 
   let isSanctioned = false;
 
   if (!!data) {
-    if (OFAC_SANCTIONED.includes(data.country_code)) {
+    const { loc } = keyValueToJSON(data);
+
+    console.log(loc);
+
+    if (OFAC_SANCTIONED.includes(loc)) {
       console.log("SANCTIONED");
 
       isSanctioned = true;
