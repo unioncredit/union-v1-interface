30b8f2e8006d85631d8da1c660884ce92e3a8a05
Jeff Reiner <theundeadghost@gmail.com>
2020-06-24 13:28:08 +0000

    Responsive Toast component

--- components/toasts.js
+++ components/toasts.js
@@ -2,7 +2,7 @@ import { Failure, Loading, Pending, Success } from "svgs/Alerts";
 
 export const ToastBody = ({ body, onDismiss }) => (
   <div className="flex justify-between items-center">
-    <p className="mr-12 text-sm truncate leading-snug">{body}</p>
+    <p className="mr-4 sm:mr-12 text-sm truncate leading-snug">{body}</p>
     <button
       className="text-sm font-medium underline focus:outline-none"
       onClick={onDismiss}
@@ -13,7 +13,7 @@ export const ToastBody = ({ body, onDismiss }) => (
     <style jsx global>{`
       .ct-toast {
         justify-content: flex-start;
-        padding: calc(1.5rem - 1px);
+        padding: calc(1rem - 1px);
         border-radius: 0.5rem;
         box-shadow: 0px 6px 16px rgba(0, 0, 0, 0.04);
         border: 1px solid #f3f4f7;
@@ -22,6 +22,7 @@ export const ToastBody = ({ body, onDismiss }) => (
       @media screen and (min-width: 40em) {
         .ct-toast {
           min-width: 440px;
+          padding: calc(1.5rem - 1px);
         }
       }
 
