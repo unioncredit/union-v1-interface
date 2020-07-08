f3426697e7f4ee65b784102a9993d29d6c2e8ad5
Jeff Reiner <theundeadghost@gmail.com>
2020-07-08 13:11:09 +0200

    Add in logic for ERC 1155 and fetching token balance

--- hooks/useGoldenTicket.js
+++ hooks/useGoldenTicket.js
@@ -1,15 +1,32 @@
 import { useWeb3React } from "@web3-react/core";
-import { isAddress } from "@ethersproject/address";
+import { GOLDEN_TICKET_NFT_ADDRESS } from "constants/variables";
 import useSWR from "swr";
+import useContract from "./useContract";
 
-const hasGoldenTicket = (account) => async (_) => {
+const GOLDEN_TICKET = {
+  address: GOLDEN_TICKET_NFT_ADDRESS,
   /**
-   * Hook up to check if account has a certain contract
-   *   NFT in their wallet before allowing access to the app
+   * Uses new Human-Readable ABI Standard
+   * @see https://docs.ethers.io/v5/api/utils/abi/formats/#abi-formats--human-readable-abi
+   *
+   * ERC-1155 Multi Token Standard
+   * @see https://eips.ethereum.org/EIPS/eip-1155
    */
-  let accountHasNFT = false;
+  abi: [
+    "function balanceOf(address _owner, uint256 _id) external view returns (uint256)",
+  ],
+  /**
+   * ERC-1155 Token ID
+   */
+  id: "",
+};
 
-  if (accountHasNFT) return true;
+const hasGoldenTicket = (contract) => async (_, account) => {
+  const balanceOf = await contract.balanceOf(account, GOLDEN_TICKET.id);
+
+  console.log({ balanceOf });
+
+  if (balanceOf) return true;
 
   return false;
 };
@@ -17,11 +34,14 @@ const hasGoldenTicket = (account) => async (_) => {
 export default function useGoldenTicket() {
   const { account, library } = useWeb3React();
 
-  const shouldFetch = isAddress(account) && !!library;
+  const ticketContract = useContract(GOLDEN_TICKET.address, GOLDEN_TICKET.abi);
+
+  const shouldFetch =
+    typeof account === "string" && !!library && !!ticketContract;
 
   const { data } = useSWR(
-    shouldFetch ? "GoldenTicket" : null,
-    hasGoldenTicket(account)
+    shouldFetch ? ["GoldenTicket", account] : null,
+    hasGoldenTicket(ticketContract)
   );
 
   return data;
