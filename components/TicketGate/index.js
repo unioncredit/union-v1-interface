b03271c74302ee87d724d99a30f1e0aa52195400
Jeff Reiner <gone@hey.com>
2020-07-14 17:58:47 +0200

    Work on the TicketGate

--- components/TicketGate/index.js
+++ components/TicketGate/index.js
@@ -9,17 +9,23 @@ const TicketGate = ({ children }) => {
   return (
     <div className="bg-white border max-w-md mx-auto rounded p-4 sm:p-6 md:p-8 text-center">
       <div className="flex justify-center">
-        <img src="/images/logged-out.svg" alt="" />
+        <span
+          aria-label="Golden Ticket"
+          className="text-10xl leading-none -my-6"
+          role="image"
+        >
+          🎟
+        </span>
       </div>
 
       <h1 className="mb-4 mt-6">No Golden Ticket?</h1>
       <p className="text-lg leading-6 text-grey-pure mb-8">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt vero
-        amet natus consectetur.
+        Right now you'll need a limited edition crypto-collectible to be able to
+        access Union during the alpha program.
       </p>
 
       <Button full href="/waitlist">
-        Learn how to win one
+        Buy some chocolate
       </Button>
     </div>
   );
