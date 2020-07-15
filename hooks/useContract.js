37320f5ad8d7c1b7ea95609afa200d9a4faff957
Jeff Reiner <gone@hey.com>
2020-07-15 12:52:06 +0200

    Refactor useContract

--- hooks/useContract.js
+++ hooks/useContract.js
@@ -1,14 +1,16 @@
 import { isAddress } from "@ethersproject/address";
 import { Contract } from "@ethersproject/contracts";
 import { useWeb3React } from "@web3-react/core";
-import { useAutoMemo } from "hooks.macro";
+import { useMemo } from "react";
 
 export default function useContract(address, ABI) {
   const { library, account } = useWeb3React();
 
-  return useAutoMemo(() =>
-    isAddress(address) && !!ABI && !!library
-      ? new Contract(address, ABI, library.getSigner(account))
-      : undefined
+  return useMemo(
+    () =>
+      isAddress(address) && !!ABI && !!library
+        ? new Contract(address, ABI, library.getSigner(account))
+        : undefined,
+    [address, ABI, library, account]
   );
 }
