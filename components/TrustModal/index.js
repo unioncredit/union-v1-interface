165d55a4e15ebc37cb776a10cd03da84d5f89a10
Jeff Reiner <gone@hey.com>
2020-07-17 17:54:44 +0200

    Rework handling trustmodal if not a member yet

--- components/TrustModal/index.js
+++ components/TrustModal/index.js
@@ -1,6 +1,7 @@
 import { isAddress } from "@ethersproject/address";
 import { parseUnits } from "@ethersproject/units";
 import { useWeb3React } from "@web3-react/core";
+import { useGetInvitedModalToggle } from "components/GetInvitedModal/state";
 import useAddressLabels from "hooks/useAddressLabels";
 import useCurrentToken from "hooks/useCurrentToken";
 import useIsMember from "hooks/useIsMember";
@@ -21,6 +22,8 @@ const TrustModal = ({ initialAddress, initialTrust }) => {
   const open = useTrustModalOpen();
   const toggle = useTrustModalToggle();
 
+  const toggleGetInvitedModal = useGetInvitedModalToggle();
+
   const { handleSubmit, register, formState, errors, reset } = useForm();
 
   useEffect(() => reset(), [open]);
@@ -31,7 +34,7 @@ const TrustModal = ({ initialAddress, initialTrust }) => {
 
   const addToast = useToast();
 
-  const isMember = useIsMember();
+  const { data: isMember = null } = useIsMember();
 
   const memberContract = useMemberContract();
 
@@ -46,39 +49,25 @@ const TrustModal = ({ initialAddress, initialTrust }) => {
     const amount = parseUnits(rawAmount, 18).toString();
 
     if (data.label) {
-      setLabel(address, data.label);
+      await setLabel(address, data.label);
     }
 
     try {
-      let tx, estimate;
+      let gasLimit;
 
-      if (isMember === true) {
-        try {
-          estimate = await memberContract.estimateGas.updateTrust(
-            address,
-            curToken,
-            amount
-          );
-        } catch (error) {
-          estimate = 300000;
-        }
-        tx = await memberContract.updateTrust(address, curToken, amount, {
-          gasLimit: estimate,
-        });
-      } else {
-        try {
-          estimate = await memberContract.estimateGas.addTrust(
-            address,
-            curToken,
-            amount
-          );
-        } catch (error) {
-          estimate = 500000;
-        }
-        tx = await memberContract.addTrust(address, curToken, amount, {
-          gasLimit: estimate,
-        });
+      try {
+        gasLimit = await memberContract.estimateGas.updateTrust(
+          address,
+          curToken,
+          amount
+        );
+      } catch (error) {
+        gasLimit = 300000;
       }
+
+      const tx = await memberContract.updateTrust(address, curToken, amount, {
+        gasLimit,
+      });
 
       const { hide: hidePending } = addToast(
         FLAVORS.TX_PENDING(tx.hash, chainId)
@@ -117,75 +106,90 @@ const TrustModal = ({ initialAddress, initialTrust }) => {
     }
   };
 
+  const handleLearnMore = () => {
+    toggle();
+    toggleGetInvitedModal();
+  };
+
   return (
     <Modal isOpen={open} onDismiss={toggle}>
       <ModalHeader title="Vouch for a member" onDismiss={toggle} />
 
-      <form
-        method="POST"
-        onSubmit={handleSubmit(onSubmit)}
-        className="px-4 py-6 sm:px-6 sm:py-8"
-      >
-        <Input
-          autoFocus={!initialAddress}
-          className="mb-4"
-          defaultValue={initialAddress}
-          id="address"
-          label="Address"
-          name="address"
-          placeholder="Enter address"
-          error={errors.address}
-          ref={register({
-            required: "Please fill out this field",
-            validate: (value) =>
-              isAddress(value) ? true : "Please input a valid Ethereum address",
-          })}
-        />
+      <div className="px-4 py-6 sm:px-6 sm:py-8">
+        {isMember !== null && isMember === false && (
+          <p className="mb-8 -mt-1">
+            <strong>Note</strong>: You are not yet a Union member, you cannot
+            yet vouch for another address.{" "}
+            <button onClick={handleLearnMore} className="underline font-medium">
+              Learn more
+            </button>
+          </p>
+        )}
 
-        <Input
-          className="mb-4"
-          id="label"
-          label="Label (Optional)"
-          name="label"
-          placeholder="Label this address"
-          error={errors.label}
-          tip="Labels are always stored locally on your device"
-          ref={register}
-        />
+        <form method="POST" onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            autoFocus={!initialAddress}
+            className="mb-4"
+            defaultValue={initialAddress}
+            id="address"
+            label="Address"
+            name="address"
+            placeholder="Enter address"
+            error={errors.address}
+            ref={register({
+              required: "Please fill out this field",
+              validate: (value) =>
+                isAddress(value)
+                  ? true
+                  : "Please input a valid Ethereum address",
+            })}
+          />
 
-        <Input
-          autoFocus={!!initialAddress}
-          chip="DAI"
-          className="mb-4"
-          defaultValue={initialTrust > 0 ? initialTrust : undefined}
-          id="amount"
-          label="Trust amount"
-          name="amount"
-          placeholder="0.00"
-          step={0.01}
-          tip="The amount you trust this address to borrow and be able to repay."
-          type="number"
-          error={errors.amount}
-          ref={register({
-            required: "Please fill out this field",
-            min: {
-              value: 1.0,
-              message: "Value must be greater than or equal to 1.00",
-            },
-          })}
-        />
+          <Input
+            className="mb-4"
+            id="label"
+            label="Label (Optional)"
+            name="label"
+            placeholder="Label this address"
+            error={errors.label}
+            tip="Labels are always stored locally on your device"
+            ref={register}
+          />
 
-        <div className="mt-20">
-          <Button
-            full
-            type="submit"
-            disabled={isSubmitting || !isDirty}
-            submitting={isSubmitting}
-          >
-            Confirm
-          </Button>
-        </div>
-      </form>
+          <Input
+            autoFocus={!!initialAddress}
+            chip="DAI"
+            className="mb-4"
+            defaultValue={initialTrust > 0 ? initialTrust : undefined}
+            id="amount"
+            label="Trust amount"
+            name="amount"
+            placeholder="0.00"
+            step={0.01}
+            tip="The amount you trust this address to borrow and be able to repay."
+            type="number"
+            error={errors.amount}
+            ref={register({
+              required: "Please fill out this field",
+              min: {
+                value: 1.0,
+                message: "Value must be greater than or equal to 1.00",
+              },
+            })}
+          />
+
+          <div className="mt-20">
+            <Button
+              full
+              type="submit"
+              disabled={isMember === false || isSubmitting || !isDirty}
+              submitting={isSubmitting}
+            >
+              Confirm
+            </Button>
+          </div>
+        </form>
+      </div>
     </Modal>
   );
 };
--- hooks/usePopTrustModal.js
+++ hooks/usePopTrustModal.js
@@ -1,32 +1,15 @@
-import { useGetInvitedModalToggle } from "components/GetInvitedModal/state";
 import { useTrustModalToggle } from "components/TrustModal/state";
 import { useRouter } from "next/router";
-import { Fragment, useEffect } from "react";
-import useIsMember from "./useIsMember";
-import useToast from "./useToast";
+import { useEffect } from "react";
 
 export default function usePopTrustModal() {
   const { query } = useRouter();
 
-  const { data: isMember = false } = useIsMember();
-
-  const toggleGetInvitedModal = useGetInvitedModalToggle();
   const toggleTrustModal = useTrustModalToggle();
-  const addToast = useToast();
 
   useEffect(() => {
-    if ((query.address || query.trust) && isMember === true) {
+    if (query.address || query.trust) {
       toggleTrustModal();
-    }
-
-    if ((query.address || query.trust) && isMember === false) {
-      addToast({
-        body: <Fragment>You are not yet a Union member</Fragment>,
-        type: "warn",
-        hideAfter: 30,
-      });
-
-      toggleGetInvitedModal();
     }
   }, []);
 }
