d4137a372d1a5bc4969badbf6d4ba59017f4f646
Jeff Reiner <gone@hey.com>
2020-07-21 13:46:11 +0200

    Refactor borrow

---
+++ hooks/payables/useBorrow.js
@@ -0,0 +1,38 @@
+import { Contract } from "@ethersproject/contracts";
+import { parseUnits } from "@ethersproject/units";
+import { useWeb3React } from "@web3-react/core";
+import LENDING_MARKET_ABI from "constants/abis/lendingMarket.json";
+import useCurrentToken from "hooks/useCurrentToken";
+import useMarketRegistryContract from "hooks/useMarketRegistryContract";
+import { useCallback } from "react";
+
+export default function useBorrow() {
+  const { library } = useWeb3React();
+  const tokenAddress = useCurrentToken();
+  const marketRegistryContract = useMarketRegistryContract();
+
+  return useCallback(async (amount) => {
+    const marketAddress = await marketRegistryContract.tokens(tokenAddress);
+
+    const lendingMarketContract = new Contract(
+      marketAddress,
+      LENDING_MARKET_ABI,
+      library.getSigner()
+    );
+
+    const borrowAmount = parseUnits(String(amount), 18);
+
+    let gasLimit;
+    try {
+      gasLimit = await lendingMarketContract.estimateGas.borrow(
+        borrowAmount.toString()
+      );
+    } catch (err) {
+      gasLimit = 5000000;
+    }
+
+    return lendingMarketContract.borrow(borrowAmount.toString(), {
+      gasLimit,
+    });
+  }, []);
+}
