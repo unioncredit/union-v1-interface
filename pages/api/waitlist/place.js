68b991e319a43b5a23cae0f480768d6cbefff952
Jeff Reiner <gone@hey.com>
2020-07-22 10:52:28 +0200

    Only allow POST requests

--- pages/api/waitlist/place.js
+++ pages/api/waitlist/place.js
@@ -1,29 +1,33 @@
 import { graphql } from "lib/fauna/client";
 
 export default async function place(req, res) {
-  const { email } = req.body;
+  if (req.method === "POST") {
+    const { email } = req.body;
 
-  try {
-    if (!email) throw new Error("'email' is Required");
+    try {
+      if (!email) throw new Error("'email' is Required");
 
-    const query = /* GraphQL */ `
-      query place($email: String!) {
-        findPlaceInLine(email: $email) {
-          position
-          total
-          hasGoldenTicket
+      const query = /* GraphQL */ `
+        query place($email: String!) {
+          findPlaceInLine(email: $email) {
+            position
+            total
+            hasGoldenTicket
+          }
         }
-      }
-    `;
+      `;
 
-    const variables = {
-      email,
-    };
+      const variables = {
+        email,
+      };
 
-    const data = await graphql.request(query, variables);
+      const data = await graphql.request(query, variables);
 
-    res.status(200).json({ success: true, result: data.findPlaceInLine });
-  } catch (e) {
-    res.status(400).json({ success: false, error: e.message });
+      res.status(200).json({ success: true, result: data.findPlaceInLine });
+    } catch (e) {
+      res.status(400).json({ success: false, error: e.message });
+    }
+  } else {
+    res.status(405).send("Method Not Allowed");
   }
 }
--- pages/api/waitlist/signup.js
+++ pages/api/waitlist/signup.js
@@ -1,45 +1,49 @@
 import { graphql } from "lib/fauna/client";
 
 export default async function signup(req, res) {
-  const { email } = req.body;
+  if (req.method === "POST") {
+    const { email } = req.body;
 
-  try {
-    if (!email) throw new Error("'email' is Required");
+    try {
+      if (!email) throw new Error("'email' is Required");
 
-    const query = /* GraphQL */ `
-      mutation signup($email: String!) {
-        createSignup(data: { email: $email, sentGoldenTicket: false }) {
-          id: _id
-          timestamp: _ts
+      const query = /* GraphQL */ `
+        mutation signup($email: String!) {
+          createSignup(data: { email: $email, sentGoldenTicket: false }) {
+            id: _id
+            timestamp: _ts
+          }
         }
-      }
-    `;
+      `;
 
-    const variables = {
-      email,
-    };
+      const variables = {
+        email,
+      };
 
-    const data = await graphql.request(query, variables);
+      const data = await graphql.request(query, variables);
 
-    res.status(200).json({
-      success: true,
-      result: {
-        timestamp: data.createSignup.timestamp,
-      },
-    });
-  } catch (e) {
-    if (e.message.includes("Instance is not unique")) {
-      res.status(422).json({
-        success: false,
-        error: "You're already signed up",
+      res.status(200).json({
+        success: true,
+        result: {
+          timestamp: data.createSignup.timestamp,
+        },
       });
+    } catch (e) {
+      if (e.message.includes("Instance is not unique")) {
+        res.status(422).json({
+          success: false,
+          error: "You're already signed up",
+        });
 
-      return;
+        return;
+      }
+
+      res.status(404).json({
+        success: false,
+        error: e.message,
+      });
     }
-
-    res.status(404).json({
-      success: false,
-      error: e.message,
-    });
+  } else {
+    res.status(405).send("Method Not Allowed");
   }
 }
