242e40deec71a49972c92a54f2350bcd74aaf7e7
Jeff Reiner <gone@hey.com>
2020-07-28 16:08:32 +0200

    Hooking up Skeleton for vouch bar

--- views/vouch.js
+++ views/vouch.js
@@ -10,6 +10,7 @@ import useCreditLimit from "hooks/useCreditLimit";
 import useIsMember from "hooks/useIsMember";
 import useVouchData from "hooks/useVouchData";
 import { Fragment } from "react";
+import Skeleton from "styled-jsx-loading-skeleton";
 import getVouchBarData from "util/getVouchBarData";
 import { roundDown } from "util/numbers";
 
@@ -19,10 +20,9 @@ export default function VouchView() {
   const toggleCreditRequestModal = useCreditRequestModalToggle();
 
   const { data: vouchData } = useVouchData();
-
-  const { data: creditLimit = 0 } = useCreditLimit();
-
   const vouchBarSlices = getVouchBarData(vouchData);
+
+  const { data: creditLimit } = useCreditLimit();
 
   return (
     <Fragment>
@@ -32,8 +32,9 @@ export default function VouchView() {
         <div className="flex justify-between mb-6">
           <LabelPair
             label="Real-time available credit"
-            value={roundDown(creditLimit)}
-            valueType="DAI"
+            {...(creditLimit
+              ? { value: roundDown(creditLimit), valueType: "DAI" }
+              : { value: <Skeleton width={72} /> })}
             large
           />
 
@@ -44,7 +45,13 @@ export default function VouchView() {
           </div>
         </div>
 
-        <VouchBar className="mb-10" slices={vouchBarSlices} />
+        <div className="mb-10">
+          {vouchData ? (
+            <VouchBar slices={vouchBarSlices} />
+          ) : (
+            <Skeleton height={64} style={{ display: "block" }} />
+          )}
+        </div>
 
         <div className="mb-6">
           <h1>Addresses who vouched for you</h1>
