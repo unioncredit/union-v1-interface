37979c77429a39c868ec94a7180dcab3d60e8252
Jeff Reiner <gone@hey.com>
2020-07-28 16:07:36 +0200

    Adding custom loading Tx

--- components/transaction.js
+++ components/transaction.js
@@ -1,4 +1,5 @@
 import { useWeb3React } from "@web3-react/core";
+import Skeleton from "styled-jsx-loading-skeleton";
 import Tx from "svgs/Tx";
 import getEtherscanLink from "util/getEtherscanLink";
 
@@ -55,4 +56,32 @@ const Transaction = ({
     </a>
   );
 };
+
 export default Transaction;
+
+export const TransactionSkeleton = () => (
+  <div className="mt-4 bg-white border rounded p-4 sm:p-6 flex items-center focus:shadow-outline focus:outline-none hover:bg-border-light transition-colors duration-150">
+    <Skeleton circle width={48} height={48} style={{ display: "block" }} />
+
+    <div className="flex-1 mx-4">
+      <p className="hidden sm:block mb-2 leading-none">
+        <Skeleton width={252} />
+      </p>
+      <p className="block sm:hidden mb-2 leading-none">
+        <Skeleton width={52} />
+      </p>
+      <p className="hidden sm:block font-normal leading-none">
+        <Skeleton width={160} />
+      </p>
+      <p className="block sm:hidden font-normal leading-none">
+        <Skeleton width={70} />
+      </p>
+    </div>
+
+    <div>
+      <p>
+        <Skeleton width={72} />
+      </p>
+    </div>
+  </div>
+);
