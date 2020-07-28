638d92214e5a211138dc484444eedf6c938c298d
Jeff Reiner <gone@hey.com>
2020-07-28 10:58:46 +0200

    Add hexlify

--- hooks/usePersonalSign.js
+++ hooks/usePersonalSign.js
@@ -1,7 +1,11 @@
 import { useWeb3React } from "@web3-react/core";
 
+const hexlify = (message) =>
+  "0x" + Buffer.from(message, "utf8").toString("hex");
+
 export default function usePersonalSign() {
   const { library, account } = useWeb3React();
 
-  return async (message) => library.send("personal_sign", [message, account]);
+  return async (message) =>
+    library.send("personal_sign", [hexlify(message), account]);
 }
