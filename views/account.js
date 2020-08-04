0928c5bbe792acd294ed256609a67d0c316176d0
Jeff Reiner <gone@hey.com>
2020-08-04 10:48:38 +0200

    Remove email from account page Add 3Box Profile edit link

--- views/account.js
+++ views/account.js
@@ -1,43 +1,17 @@
 import { useWeb3React } from "@web3-react/core";
-import Button from "components/button";
 import Input from "components/input";
-import { MESSAGE } from "constants/variables";
+import use3BoxPublicData from "hooks/use3BoxPublicData";
 import useCopy from "hooks/useCopy";
-import { Fragment, useEffect } from "react";
-import { useForm } from "react-hook-form";
-import errorMessages from "text/errorMessages";
-import EMAIL_REGEX from "util/emailRegex";
+import { Fragment } from "react";
 
 export default function AccountView() {
-  const { library, account } = useWeb3React();
-
-  const { register, handleSubmit, formState, errors, reset } = useForm();
-
-  useEffect(() => reset(), [open]);
-
-  const { isDirty, isSubmitting } = formState;
+  const { account } = useWeb3React();
 
   const [isCopied, copy] = useCopy();
-
   const handleCopy = () => copy(account);
 
-  const onSubmit = async (values) => {
-    const { email } = values;
-
-    try {
-      if (!email) throw new Error("`email` is required");
-
-      const signer = library.getSigner();
-
-      const signature = await signer.signMessage(MESSAGE);
-
-      /**
-       * @todo Post email to DB here with a key / value pair of address / email
-       */
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  const { data, error } = use3BoxPublicData(account);
+  const has3BoxName = !!data && !error && data?.name;
 
   return (
     <Fragment>
@@ -45,46 +19,29 @@ export default function AccountView() {
         <div className="bg-white border max-w-md mx-auto rounded p-4 sm:p-6 md:p-8">
           <h1 className="mb-6">My account</h1>
 
-          <form method="POST" onSubmit={handleSubmit(onSubmit)}>
-            <Input
-              autoComplete="email"
-              className="mb-10"
-              id="email"
-              label="Your email"
-              name="email"
-              type="email"
-              placeholder="name@email.com"
-              error={errors.email}
-              ref={register({
-                required: errorMessages.required,
-                pattern: {
-                  value: EMAIL_REGEX,
-                  message: errorMessages.validEmail,
-                },
-              })}
-            />
+          <Input
+            className="mb-6"
+            label="Your address"
+            readOnly
+            setMax={handleCopy}
+            setMaxLabel=""
+            setMaxValue={isCopied ? "Copied!" : "Copy"}
+            value={account}
+          />
 
-            <div className="mb-16">
-              <p className="mb-2 text-type-light">Your address</p>
-              <button
-                className="font-medium py-1 w-full truncate text-sm text-left focus:outline-none hover:underline"
-                onClick={handleCopy}
-                title={account}
-                type="button"
+          {has3BoxName && (
+            <p>
+              Looks like you're using 3box! Update your public profile on{" "}
+              <a
+                className="underline"
+                href={`https://3box.io/${account}/edit`}
+                rel="noopener noreferrer"
+                target="_blank"
               >
-                {isCopied ? "Copied!" : account}
-              </button>
-            </div>
-
-            <Button
-              full
-              type="submit"
-              disabled={isSubmitting || !isDirty}
-              submitting={isSubmitting}
-            >
-              Save changes
-            </Button>
-          </form>
+                3Box
+              </a>
+            </p>
+          )}
         </div>
       </div>
     </Fragment>
