712e0e986936549b7932983950a9bc145dcfe5a7
Jeff Reiner <gone@hey.com>
2020-08-04 15:13:28 +0200

    Reworking Toasts, adding getReceipt function

--- hooks/useToast.js
+++ hooks/useToast.js
@@ -1,30 +1,6 @@
 import cogoToast from "cogo-toast";
-import { ToastBody, TOAST_ICONS } from "components/toasts";
-import { Fragment, useCallback } from "react";
-import getEtherscanLink from "util/getEtherscanLink";
-import { useWeb3React } from "@web3-react/core";
-
-const ToastContent = ({ message, hash }) => {
-  const { chainId } = useWeb3React();
-
-  if (hash)
-    return (
-      <Fragment>
-        {message}
-        <br />
-        <a
-          className="underline"
-          href={getEtherscanLink(chainId, hash, "TRANSACTION")}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          View on Etherscan
-        </a>
-      </Fragment>
-    );
-
-  return message;
-};
+import { ToastBody, ToastContent, TOAST_ICONS } from "components/toasts";
+import { useCallback } from "react";
 
 export const FLAVORS = {
   TX_WAITING: {
@@ -68,34 +44,32 @@ export const FLAVORS = {
   }),
 };
 
-export default function useToast() {
-  const addToast = useCallback(
-    /**
-     * @name addToast
-     * @param {Object} options
-     * @param {any} options.body
-     * @param {("success"|"info"|"loading"|"warn"|"error")} options.type
-     * @param {Number} options.hideAfter
-     */
-    ({ body, type = "success", hideAfter = 6 }) => {
-      const { hide } = cogoToast[type](
-        <ToastBody body={body} onDismiss={() => hide()} />,
-        {
-          position: "bottom-right",
-          bar: {
-            size: "1px",
-            style: "solid",
-            color: "#f3f4f7",
-          },
-          hideAfter,
-          renderIcon: TOAST_ICONS[type],
-        }
-      );
-
-      return { hide };
-    },
-    []
+/**
+ * @name addToast
+ *
+ * @param {Object} options
+ * @param {any} options.body
+ * @param {("success"|"info"|"loading"|"warn"|"error")} options.type
+ * @param {Number} options.hideAfter
+ */
+export const addToast = ({ body, type = "success", hideAfter = 6 }) => {
+  const { hide } = cogoToast[type](
+    <ToastBody body={body} onDismiss={() => hide()} />,
+    {
+      renderIcon: TOAST_ICONS[type],
+      position: "bottom-right",
+      hideAfter,
+      bar: {
+        size: "1px",
+        style: "solid",
+        color: "#f3f4f7",
+      },
+    }
   );
 
-  return addToast;
+  return { hide };
+};
+
+export default function useToast() {
+  return useCallback(addToast, []);
 }
