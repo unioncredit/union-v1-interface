8a3b90dd7a3e4f556aebdcfb479ae82274b5eb4b
Jeff Reiner <gone@hey.com>
2020-08-04 15:04:53 +0200

    Cleaning up payable hooks with proper return types for easier using

---
+++ hooks/payables/useWithdrawRewards.js
@@ -0,0 +1,20 @@
+import { useWeb3React } from "@web3-react/core";
+import useCurrentToken from "hooks/useCurrentToken";
+import useUnionContract from "hooks/useUnionContract";
+import { useCallback } from "react";
+
+export default function useWithdrawRewards() {
+  const { account } = useWeb3React();
+  const tokenAddress = useCurrentToken();
+  const unionContract = useUnionContract();
+
+  return useCallback(
+    /**
+     * @returns {Promise<import("@ethersproject/abstract-provider").TransactionResponse>}
+     */
+    async () => {
+      return unionContract.withdrawRewards(account, tokenAddress);
+    },
+    []
+  );
+}
