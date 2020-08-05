52869aed7c1b85efa9c049511d0471885f2ac862
Jeff Reiner <gone@hey.com>
2020-08-05 10:34:16 +0200

    More editable looking Account screen

--- views/account.js
+++ views/account.js
@@ -6,6 +6,8 @@ import useENSName from "hooks/useENSName";
 import { Fragment } from "react";
 import getIPFSAssetLink from "util/getIPFSAssetLink";
 import truncateAddress from "util/truncateAddress";
+import Input from "components/input";
+import Tooltip from "@reach/tooltip";
 
 export default function AccountView() {
   const { account } = useWeb3React();
@@ -24,62 +26,72 @@ export default function AccountView() {
       <div className="container">
         <div className="max-w-sm mx-auto">
           <h1 className="h-12 leading-12 mb-4">My account</h1>
-          <div className="bg-white border rounded px-4 py-8 sm:px-6 sm:py-10">
-            <div className="flex justify-center">
-              {has3BoxProfileImage ? (
-                <img
-                  alt={account}
-                  async
-                  className="rounded-full"
-                  decoding="async"
-                  loading="lazy"
-                  src={getIPFSAssetLink(data.image)}
-                  style={{ height: 72, width: 72 }}
-                />
-              ) : (
-                <Identicon address={account} extraLarge />
-              )}
+          <div className="bg-white border rounded p-4 sm:p-6">
+            <div className="mb-4 sm:mb-6">
+              <div className="flex items-center">
+                {has3BoxProfileImage ? (
+                  <img
+                    alt={account}
+                    async
+                    className="rounded-full"
+                    decoding="async"
+                    loading="lazy"
+                    src={getIPFSAssetLink(data.image)}
+                    style={{ height: 72, width: 72 }}
+                  />
+                ) : (
+                  <Identicon address={account} extraLarge />
+                )}
+                <div className="ml-4">
+                  <p
+                    className="text-xl font-semibold  leading-tight"
+                    title={account}
+                  >
+                    {truncateAddress(account, 6)}
+                  </p>
+                  <button
+                    className="text-sm font-medium underline focus:outline-none"
+                    onClick={handleCopy}
+                    type="button"
+                  >
+                    {isCopied ? "Copied" : "Copy Address"}
+                  </button>
+                </div>
+              </div>
             </div>
 
-            <div className="mt-4 text-center">
-              <p className="text-lg font-semibold">
-                {data?.name ?? ENSName ?? truncateAddress(account, 6)}
-              </p>
-              <button
-                type="button"
-                onClick={handleCopy}
-                className="focus:outline-none leading-none font-medium text-type-light"
-                title={account}
-              >
-                {isCopied ? "Copied!" : ENSName ?? truncateAddress(account, 6)}
-              </button>
-            </div>
-
-            {has3BoxName ? (
-              <p className="mt-8 text-center">
-                Looks like you're using 3box! Update your public profile on{" "}
-                <a
-                  className="underline"
-                  href={`https://3box.io/${account}/edit`}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  3Box
-                </a>
-              </p>
-            ) : (
-              <p className="mt-8 text-center">
-                Setup your public profile{" "}
-                <a
-                  className="underline"
-                  href={`https://3box.io/${account}/edit`}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  on 3Box
-                </a>
-              </p>
-            )}
+            <Input
+              label="Your name"
+              readOnly
+              value={data?.name ?? ENSName}
+              tip={
+                has3BoxName ? (
+                  <span>
+                    Looks like you're using 3box! Update your public profile{" "}
+                    <a
+                      className="underline"
+                      href={`https://3box.io/${account}/edit`}
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      on 3Box
+                    </a>
+                  </span>
+                ) : (
+                  <span>
+                    Setup your public profile{" "}
+                    <a
+                      className="underline"
+                      href={`https://3box.io/${account}/edit`}
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      on 3Box
+                    </a>
+                  </span>
+                )
+              }
+            />
           </div>
         </div>
       </div>
