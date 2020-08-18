bfd2745f557bf344648f10f1de9422f202d9191e
Jeff Reiner <gone@hey.com>
2020-08-18 18:17:33 +0200

    Working on MVP stats

--- views/statistics.js
+++ views/statistics.js
@@ -3,14 +3,73 @@ import { Fragment } from "react";
 export default function StatsView() {
   return (
     <Fragment>
-      <div className="container">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur,
-          praesentium aliquid. Cumque, dicta tempore libero perferendis
-          consequuntur odit! Facere dolore dignissimos hic itaque id laboriosam
-          cumque distinctio eaque quos deserunt.
-        </p>
-      </div>
+      <section>
+        <div className="container">
+          <h1>Union Stats</h1>
+        </div>
+      </section>
+
+      <section className="pt-12 sm:pt-24">
+        <div className="container mb-8">
+          <h2>Global Lending</h2>
+        </div>
+
+        <div className="container">
+          <div className="flex divide-x">
+            <div className="flex-1 flex justify-center p-4 sm:p-6">Label</div>
+            <div className="flex-1 flex justify-center p-4 sm:p-6">Label 2</div>
+            <div className="flex-1 flex justify-center p-4 sm:p-6">Label 3</div>
+            <div className="flex-1 flex justify-center p-4 sm:p-6">Label 4</div>
+            <div className="flex-1 flex justify-center p-4 sm:p-6">Label 5</div>
+          </div>
+        </div>
+      </section>
+
+      <section className="pt-12 sm:pt-24">
+        <div className="container mb-8">
+          <h2>Asset Management</h2>
+        </div>
+
+        <div className="container">
+          <div className="flex divide-x">
+            <div className="flex-1 flex justify-center p-4 sm:p-6">Label</div>
+            <div className="flex-1 flex justify-center p-4 sm:p-6">Label 2</div>
+          </div>
+        </div>
+      </section>
+
+      <section className="pt-12 sm:pt-24">
+        <div className="container mb-8">
+          <h2>UNION Token</h2>
+        </div>
+
+        <div className="container">
+          <div className="flex divide-x">
+            <div className="flex-1 flex justify-center p-4 sm:p-6">Label</div>
+            <div className="flex-1 flex justify-center p-4 sm:p-6">Label 2</div>
+          </div>
+        </div>
+      </section>
+
+      <section className="pt-12 sm:pt-24">
+        <div className="container mb-8">
+          <h2>Current Parameter Values</h2>
+        </div>
+
+        <div className="container">
+          <div className="flex divide-x">
+            <div className="flex-1 flex justify-center p-4 sm:p-6">Label</div>
+            <div className="flex-1 flex justify-center p-4 sm:p-6">Label</div>
+            <div className="flex-1 flex justify-center p-4 sm:p-6">Label</div>
+            <div className="flex-1 flex justify-center p-4 sm:p-6">Label</div>
+            <div className="flex-1 flex justify-center p-4 sm:p-6">Label</div>
+            <div className="flex-1 flex justify-center p-4 sm:p-6">Label</div>
+            <div className="flex-1 flex justify-center p-4 sm:p-6">Label</div>
+            <div className="flex-1 flex justify-center p-4 sm:p-6">Label</div>
+            <div className="flex-1 flex justify-center p-4 sm:p-6">Label</div>
+          </div>
+        </div>
+      </section>
     </Fragment>
   );
 }
