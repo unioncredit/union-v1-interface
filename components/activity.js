91fa57105a3a170cfe21403458a772875b913247
Jeff Reiner <gone@hey.com>
2020-08-25 18:21:38 +0200

    Remove extra markup

--- components/activity.js
+++ components/activity.js
@@ -12,93 +12,87 @@ const Activity = () => {
   const { data } = useActivity();
 
   return (
-    <ul className="flex items-center">
-      <li className="relative">
-        {" "}
-        <Menu>
-          <MenuButton className="h-8 w-8 bg-white hover:bg-border-light transition-colors duration-150 items-center justify-center flex rounded focus:outline-none focus:shadow-outline">
-            <Bell pending={data && data.length > 0} />
-          </MenuButton>
+    <Menu>
+      <MenuButton className="h-8 w-8 bg-white hover:bg-border-light transition-colors duration-150 items-center justify-center flex rounded focus:outline-none focus:shadow-outline">
+        <Bell pending={data && data.length > 0} />
+      </MenuButton>
 
-          <MenuList className="mt-4 w-80">
-            <div className="p-4 border-b">
-              <p className="text-lg font-semibold leading-none">Activity</p>
-            </div>
-
-            <div className="p-4">
-              <ul className="divide-y space-y-4 -mt-4">
-                {data &&
-                  data.map((log, i) => {
-                    if (log.type === "UpdateTrust")
-                      return (
-                        <li key={i} className="text-sm pt-4">
-                          <div className="flex space-x-4">
-                            <div>
-                              <Identicon address={log.borrower} large />
-                            </div>
-                            <div>
-                              <p className="whitespace-normal leading-snug crop-snug mb-2">
-                                <strong>{truncateAddress(log.borrower)}</strong>{" "}
-                                now trusts you with{" "}
-                                <strong>{log.trustAmount} DAI</strong>
-                              </p>
-                              <div className="flex justify-between leading-none text-type-light space-x-2">
-                                <p>
-                                  <a
-                                    className="underline"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    href={getEtherscanLink(
-                                      chainId,
-                                      log.hash,
-                                      "TRANSACTION"
-                                    )}
-                                  >
-                                    See details
-                                  </a>
-                                </p>
-                                <p className="text-right">{log.date}</p>
-                              </div>
-                            </div>
+      <MenuList className="mt-4 w-80">
+        <div className="p-4 border-b">
+          <p className="text-lg font-semibold leading-none">Activity</p>
+        </div>
+        <div className="p-4">
+          <ul className="divide-y space-y-4 -mt-4">
+            {data &&
+              data.map((log, i) => {
+                if (log.type === "UpdateTrust")
+                  return (
+                    <li key={i} className="text-sm pt-4">
+                      <div className="flex space-x-4">
+                        <div>
+                          <Identicon address={log.borrower} large />
+                        </div>
+                        <div>
+                          <p className="whitespace-normal leading-snug crop-snug mb-2">
+                            <strong>{truncateAddress(log.borrower)}</strong> now
+                            trusts you with{" "}
+                            <strong>{log.trustAmount} DAI</strong>
+                          </p>
+                          <div className="flex justify-between leading-none text-type-light space-x-2">
+                            <p>
+                              <a
+                                className="underline"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                href={getEtherscanLink(
+                                  chainId,
+                                  log.hash,
+                                  "TRANSACTION"
+                                )}
+                              >
+                                See details
+                              </a>
+                            </p>
+                            <p className="text-right">{log.date}</p>
                           </div>
-                        </li>
-                      );
+                        </div>
+                      </div>
+                    </li>
+                  );
 
-                    if (log.type === "ApplyMember")
-                      return (
-                        <li key={i} className="text-sm pt-4">
-                          <div className="flex space-x-4">
-                            <div>
-                              <Pending size={32} />
-                            </div>
-                            <div>
-                              <p className="whitespace-normal leading-snug crop-snug mb-2">
-                                <strong>Congratulations</strong> you are now a
-                                member of Union.
-                              </p>
-                              <div className="flex justify-between leading-none text-type-light space-x-2">
-                                <span />
-                                <p className="text-right">{log.date}</p>
-                              </div>
-                            </div>
+                if (log.type === "ApplyMember")
+                  return (
+                    <li key={i} className="text-sm pt-4">
+                      <div className="flex space-x-4">
+                        <div>
+                          <Pending size={32} />
+                        </div>
+                        <div>
+                          <p className="whitespace-normal leading-snug crop-snug mb-2">
+                            <strong>Congratulations</strong> you are now a
+                            member of Union.
+                          </p>
+                          <div className="flex justify-between leading-none text-type-light space-x-2">
+                            <span />
+                            <p className="text-right">{log.date}</p>
                           </div>
-                        </li>
-                      );
+                        </div>
+                      </div>
+                    </li>
+                  );
 
-                    return (
-                      <li key={i}>
-                        {/* <pre>
+                return (
+                  <li key={i}>
+                    {/* <pre>
                         <code>{JSON.stringify(log, null, 2)}</code>
                       </pre> */}
-                      </li>
-                    );
-                  })}
-              </ul>
-            </div>
-          </MenuList>
-        </Menu>
-      </li>
-    </ul>
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
+      </MenuList>
+    </Menu>
   );
 };
 
