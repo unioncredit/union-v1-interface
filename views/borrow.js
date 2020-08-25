0cb23cdec75a0a4ecbfaaacf961d1545c45fb802
Jeff Reiner <gone@hey.com>
2020-08-25 10:18:03 +0200

    Revert "Spread default borrow data differently"
    This reverts commit b38c1a5441ce5cfaf58b7014ca1a0f765c8985a0.

--- views/borrow.js
+++ views/borrow.js
@@ -38,18 +38,17 @@ export default function BorrowView() {
 
   const { data: creditLimit = 0, mutate: updateCreditLimit } = useCreditLimit();
 
+  const { data: borrowData, mutate: updateBorrowData } = useBorrowData();
+
   const {
-    data: borrowData = {
-      borrowedRounded: 0,
-      interest: 0,
-      paymentDueDate: "-",
-      paymentPeriod: "-",
-      fee: 0,
-      apr: 0,
-      isOverdue: false,
-    },
-    mutate: updateBorrowData,
-  } = useBorrowData();
+    borrowedRounded = 0,
+    interest = 0,
+    paymentDueDate = "-",
+    paymentPeriod = "-",
+    fee = 0,
+    apr = 0,
+    isOverdue = false,
+  } = !!borrowData && borrowData;
 
   const formatApr = toPercent(apr, 2);
 
