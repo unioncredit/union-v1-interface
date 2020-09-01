7e7c2457ed94850e2af22e4b5e9d7e4d828895b1
Jeff Reiner <gone@hey.com>
2020-09-01 16:58:16 +0200

    Fix eslint warning about noreferrer

--- components/TweetButton/index.js
+++ components/TweetButton/index.js
@@ -1,7 +1,7 @@
 import Twitter from "svgs/Twitter";
 
 const TweetButton = ({ href, children, ...rest }) => (
-  <a href={href} rel="noopener" target="_blank" {...rest}>
+  <a href={href} rel="noopener noreferrer" target="_blank" {...rest}>
     <div className="inline-flex space-x-3">
       <Twitter />
       <span className="text-twitter font-semibold">{children}</span>
