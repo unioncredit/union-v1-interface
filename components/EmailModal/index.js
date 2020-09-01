17df2c9f712bc7523d4c63827eebc74571896d4b
Jeff Reiner <gone@hey.com>
2020-09-01 11:12:37 +0200

    Cleanup EmailModal sign

--- components/EmailModal/index.js
+++ components/EmailModal/index.js
@@ -1,26 +1,25 @@
-import { useWeb3React } from "@web3-react/core";
 import { MESSAGE } from "constants/variables";
+import usePersonalSign from "hooks/usePersonalSign";
 import { setCookie } from "nookies";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
+import errorMessages from "text/errorMessages";
 import EMAIL_REGEX from "util/emailRegex";
 import Button from "../button";
 import Input from "../input";
 import Modal from "../modal";
 import { useEmailModalOpen, useEmailModalToggle } from "./state";
-import errorMessages from "text/errorMessages";
 
 const EmailModal = () => {
   const open = useEmailModalOpen();
   const toggle = useEmailModalToggle();
 
-  const { library } = useWeb3React();
+  const sign = usePersonalSign();
 
   const { handleSubmit, register, formState, errors, reset } = useForm();
+  const { isDirty, isSubmitting } = formState;
 
   useEffect(() => reset(), [open]);
-
-  const { isDirty, isSubmitting } = formState;
 
   const onSubmit = async (values) => {
     const { email } = values;
@@ -28,13 +27,7 @@ const EmailModal = () => {
     try {
       if (!email) throw new Error("`email` is required");
 
-      const signer = library.getSigner();
-
-      const signature = await signer.signMessage(MESSAGE);
-
-      /**
-       * @todo Post email to DB here with a key / value pair of address / email
-       */
+      await sign(MESSAGE);
 
       handleToggle();
     } catch (err) {
