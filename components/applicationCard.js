151e5fe9059cfbbee73462511599a874e9f2e4cc
Jeff Reiner <gone@hey.com>
2020-09-01 11:42:34 +0200

    Small ApplicationCard improvements

--- components/applicationCard.js
+++ components/applicationCard.js
@@ -1,6 +1,5 @@
 import classNames from "classnames";
 import useTrustCountData from "hooks/useTrustCountData";
-import { useToggle } from "react-use";
 import Info from "svgs/Info";
 import { vouchingTip } from "../text/tooltips";
 import { useApplicationModalToggle } from "./ApplicationModal/state";
@@ -14,12 +13,17 @@ const ApplicationCard = () => {
 
   const { data: trustCount = 0 } = useTrustCountData();
 
+  const vouchProgressClassNames = classNames("absolute bg-pink-2-pure h-full", {
+    "w-full": trustCount >= 3,
+    "w-2/3": trustCount === 2,
+    "w-1/3": trustCount === 1,
+  });
+
   return (
     <div className="bg-pink-light border border-pink-pure rounded p-6 mb-10">
       <div className="flex flex-col md:flex-row justify-between items-start">
-        <div>
+        <div className="mb-6 md:mb-0">
           <p className="text-lg leading-snug mb-2">Become a member of Union</p>
-
           <p className="text-xl font-normal">
             <strong className="font-semibold">
               {trustCount} out of 3 members
@@ -28,24 +32,15 @@ const ApplicationCard = () => {
           </p>
         </div>
 
-        <Button
-          className="mt-6 md:mt-0"
-          onClick={
-            trustCount >= 3 ? toggleApplicationModal : toggleGetInvitedModal
-          }
-        >
-          {trustCount >= 3 ? "Become a member" : "Ask for a vouch"}
-        </Button>
+        {trustCount >= 3 ? (
+          <Button onClick={toggleApplicationModal}>Become a member</Button>
+        ) : (
+          <Button onClick={toggleGetInvitedModal}>Ask for a vouch</Button>
+        )}
       </div>
 
       <div className="mt-6 mb-4 h-5 bg-pink-2-light rounded-full w-full md:w-1/2 relative overflow-hidden">
-        <div
-          className={classNames("absolute bg-pink-2-pure h-full", {
-            "w-full": trustCount >= 3,
-            "w-2/3": trustCount === 2,
-            "w-1/3": trustCount === 1,
-          })}
-        ></div>
+        <div className={vouchProgressClassNames} />
       </div>
 
       <div className="mb-2 md:mb-0">
