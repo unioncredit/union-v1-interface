de7453b84cf2039aa67ca40cd9f3fd031b52ed44
Jeff Reiner <gone@hey.com>
2020-09-01 10:28:51 +0200

    Improving toasts

--- hooks/useToast.js
+++ hooks/useToast.js
@@ -1,7 +1,6 @@
 import cogoToast from "cogo-toast";
-import { ToastBody, TOAST_ICONS } from "components/toasts";
-import { Fragment, useCallback } from "react";
-import getEtherscanLink from "util/getEtherscanLink";
+import { ToastBody, ToastContent, TOAST_ICONS } from "components/toasts";
+import { useCallback } from "react";
 
 export const FLAVORS = {
   TX_WAITING: {
@@ -9,114 +8,28 @@ export const FLAVORS = {
     type: "loading",
     hideAfter: 0,
   },
-  TX_PENDING: (hash, chainId) => ({
-    body: (
-      <Fragment>
-        Transaction is pending.
-        {hash && chainId && (
-          <Fragment>
-            <br />
-            <a
-              className="underline"
-              href={getEtherscanLink(chainId, hash, "TRANSACTION")}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View on Etherscan
-            </a>
-          </Fragment>
-        )}
-      </Fragment>
-    ),
+  TX_PENDING: (hash) => ({
+    body: <ToastContent message="Transaction is pending." hash={hash} />,
     type: "loading",
     hideAfter: 0,
   }),
-  TX_PENDING_TOKEN: (hash, chainId) => ({
-    body: (
-      <Fragment>
-        Enabling token.
-        {hash && chainId && (
-          <Fragment>
-            <br />
-            <a
-              className="underline"
-              href={getEtherscanLink(chainId, hash, "TRANSACTION")}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View on Etherscan
-            </a>
-          </Fragment>
-        )}
-      </Fragment>
-    ),
+  TX_PENDING_TOKEN: (hash) => ({
+    body: <ToastContent message="Enabling token." hash={hash} />,
     type: "loading",
     hideAfter: 0,
   }),
-  TX_SUCCESS: (hash, chainId) => ({
-    body: (
-      <Fragment>
-        Transaction successful.
-        {hash && chainId && (
-          <Fragment>
-            <br />
-            <a
-              className="underline"
-              href={getEtherscanLink(chainId, hash, "TRANSACTION")}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View on Etherscan
-            </a>
-          </Fragment>
-        )}
-      </Fragment>
-    ),
+  TX_SUCCESS: (hash) => ({
+    body: <ToastContent message="Transaction successful." hash={hash} />,
     type: "success",
     hideAfter: 20,
   }),
-  TX_SUCCESS_ENABLED: (hash, chainId) => ({
-    body: (
-      <Fragment>
-        Token enabled successfully.
-        {hash && chainId && (
-          <Fragment>
-            <br />
-            <a
-              className="underline"
-              href={getEtherscanLink(chainId, hash, "TRANSACTION")}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View on Etherscan
-            </a>
-          </Fragment>
-        )}
-      </Fragment>
-    ),
+  TX_SUCCESS_ENABLED: (hash) => ({
+    body: <ToastContent message="Token enabled successfully." hash={hash} />,
     type: "success",
     hideAfter: 20,
   }),
-  TX_ERROR: (message = "Transaction failed", hash, chainId) => ({
-    body: (
-      <Fragment>
-        {message}
-
-        {hash && chainId && (
-          <Fragment>
-            <br />
-            <a
-              className="underline"
-              href={getEtherscanLink(chainId, hash, "TRANSACTION")}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View on Etherscan
-            </a>
-          </Fragment>
-        )}
-      </Fragment>
-    ),
+  TX_ERROR: (message = "Transaction failed", hash) => ({
+    body: <ToastContent message={message} hash={hash} />,
     type: "error",
     hideAfter: 20,
   }),
@@ -131,34 +44,32 @@ export const FLAVORS = {
   }),
 };
 
-export default function useToast() {
-  const addToast = useCallback(
-    /**
-     * @name addToast
-     * @param {Object} options
-     * @param {any} options.body
-     * @param {("success"|"info"|"loading"|"warn"|"error")} options.type
-     * @param {Number} options.hideAfter
-     */
-    ({ body, type = "success", hideAfter = 6 }) => {
-      const { hide } = cogoToast[type](
-        <ToastBody body={body} onDismiss={() => hide()} />,
-        {
-          position: "bottom-right",
-          bar: {
-            size: "1px",
-            style: "solid",
-            color: "#f3f4f7",
-          },
-          hideAfter,
-          renderIcon: TOAST_ICONS[type],
-        }
-      );
-
-      return { hide };
-    },
-    []
+/**
+ * @name addToast
+ *
+ * @param {Object} options
+ * @param {any} options.body
+ * @param {("success"|"info"|"loading"|"warn"|"error")} options.type
+ * @param {Number} options.hideAfter
+ */
+export const addToast = ({ body, type = "success", hideAfter = 6 }) => {
+  const { hide } = cogoToast[type](
+    <ToastBody body={body} onDismiss={() => hide()} />,
+    {
+      renderIcon: TOAST_ICONS[type],
+      position: "bottom-right",
+      hideAfter,
+      bar: {
+        size: "1px",
+        style: "solid",
+        color: "#f3f4f7",
+      },
+    }
   );
 
-  return addToast;
+  return { hide };
+};
+
+export default function useToast() {
+  return useCallback(addToast, []);
 }
