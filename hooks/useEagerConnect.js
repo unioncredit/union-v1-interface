b39a55fcf2ab159b4df2ada8af304d54a6ab00a1
Jeff Reiner <gone@hey.com>
2020-09-01 10:15:23 +0200

    Don't eagerConnect on homepage or waitlist

--- hooks/useEagerConnect.js
+++ hooks/useEagerConnect.js
@@ -2,16 +2,18 @@ import { useWeb3React } from "@web3-react/core";
 import { injected } from "lib/connectors";
 import { destroyCookie, parseCookies, setCookie } from "nookies";
 import { useCallback, useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 export default function useEagerConnect() {
   const cookies = parseCookies();
+  const { pathname } = useRouter();
 
   const { activate, active } = useWeb3React();
 
   const [tried, setTried] = useState(false);
 
   useEffect(() => {
-    if (cookies.eagerConnect) {
+    if (pathname !== "/" && pathname !== "/waitlist" && cookies.eagerConnect) {
       injected.isAuthorized().then((isAuthorized) => {
         if (isAuthorized) {
           activate(injected, undefined, true).catch(() => {
