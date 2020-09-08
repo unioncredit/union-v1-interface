23fb551f0dde2ad3923bef32cfe026fe65b2fb75
Jeff Reiner <gone@hey.com>
2020-09-08 16:23:24 +0200

    Working on stats page

--- views/statistics.js
+++ views/statistics.js
@@ -3,14 +3,14 @@ import { Fragment } from "react";
 export default function StatsView() {
   return (
     <Fragment>
-      <section>
+      <section className="mb-8">
         <div className="container">
           <div className="mb-8">
             <h1 className="text-3xl md:text-4xl">Union Stats</h1>
           </div>
 
-          <ul className="flex space-x-3">
-            <li>
+          <ul className="flex flex-wrap -mb-2">
+            <li className="pr-2 pb-2">
               <a
                 className="inline-block rounded py-2 px-4 font-medium bg-black-pure hover:bg-black-dark duration-150 transition-colors text-white"
                 href="#"
@@ -18,7 +18,7 @@ export default function StatsView() {
                 Global Lending
               </a>
             </li>
-            <li>
+            <li className="pr-2 pb-2">
               <a
                 className="inline-block rounded py-2 px-4 font-medium hover:bg-black-pure hover:bg-opacity-10 duration-150 transition-colors text-type-base "
                 href="#"
@@ -26,7 +26,7 @@ export default function StatsView() {
                 Asset Management
               </a>
             </li>
-            <li>
+            <li className="pr-2 pb-2">
               <a
                 className="inline-block rounded py-2 px-4 font-medium hover:bg-black-pure hover:bg-opacity-10 duration-150 transition-colors text-type-base "
                 href="#"
@@ -34,13 +34,35 @@ export default function StatsView() {
                 UNION Token
               </a>
             </li>
-            <li>
+            <li className="pr-2 pb-2">
               <a
                 className="inline-block rounded py-2 px-4 font-medium hover:bg-black-pure hover:bg-opacity-10 duration-150 transition-colors text-type-base "
                 href="#"
               >
                 Current Parameters
               </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section className="mb-8">
+        <div className="container">
+          <div className="divider" />
+        </div>
+      </section>
+
+      <section className="mb-8">
+        <div className="container">
+          <ul className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
+            <li className="border rounded p-4 sm:p-6">
+              <p className="crop-snug leading-snug mb-4">Label</p>
+              <div className="flex items-center justify-between">
+                <p className="text-xl font-semibold crop-snug leading-snug">
+                  Value
+                </p>
+                <p className="text-sm leading-none">+100%</p>
+              </div>
             </li>
           </ul>
         </div>
