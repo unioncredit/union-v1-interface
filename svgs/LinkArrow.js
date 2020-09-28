d4d867458dc818c92b18b31e04cb5ec29a235214
Jeff Reiner <gone@hey.com>
2020-09-28 11:43:52 +0200

    Build GovernanceNavigation

---
+++ svgs/LinkArrow.js
@@ -0,0 +1,12 @@
+const LinkArrow = ({ size = 16 }) => (
+  <svg width={size} height={size} viewBox="0 0 16 16" fill="none">
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M10.707 3.293L15.914 8.5l-5.207 5.207-1.414-1.414L12.086 9.5H1v-2h11.086L9.293 4.707l1.414-1.414z"
+      fill="#F4998E"
+    />
+  </svg>
+);
+
+export default LinkArrow;
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,11 @@ module.exports = {
         pure: "#F9C4BE",
         light: "#F7EEF0",
       },
+      "pink-3": {
+        dark: "#C6A7A7",
+        pure: "#F4998E",
+        light: "#FCEDED",
+      },
       black: {
         pure: "#032437",
         light: "#C5CED5",
@@ -98,6 +103,7 @@ module.exports = {
         app: "-35px 1.16px 74px rgba(207, 192, 192, 0.16)",
         input: "0px 0px 12px rgba(19, 44, 106, 0.1)",
         smooth: `0px 100px 80px rgba(0, 0, 0, 0.07), 0px 41.7776px 33.4221px rgba(0, 0, 0, 0.0503198), 0px 22.3363px 17.869px rgba(0, 0, 0, 0.0417275), 0px 12.5216px 10.0172px rgba(0, 0, 0, 0.035), 0px 6.6501px 5.32008px rgba(0, 0, 0, 0.0282725), 0px 2.76726px 2.21381px rgba(0, 0, 0, 0.0196802)`,
+        "governance-nav": "0px 0px 8px #DEB9B9",
       },
       spacing: {
         "2px": "2px",
