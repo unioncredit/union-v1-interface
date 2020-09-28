90ed78a5a19eef9e8d53e72701c4aef50be90bfb
Jeff Reiner <gone@hey.com>
2020-09-28 13:21:46 +0200

    Responsive stats

--- components/GovernanceStatistics/index.js
+++ components/GovernanceStatistics/index.js
@@ -2,7 +2,7 @@ import GovernanceStatistic from "components/GovernanceStatistic";
 
 const GovernanceStatistics = () => {
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
       <GovernanceStatistic
         label={"Default rate"}
         value={"15%"}
