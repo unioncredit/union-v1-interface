faeeb8e567f376a7cd5ad1ef0dccccfc816a2966
Jeff Reiner <gone@hey.com>
2020-10-06 12:49:12 +0200

    Hooking up some hooks for Governance

---
+++ hooks/governance/useAllProposalData.js
@@ -0,0 +1,104 @@
+import { formatUnits } from "@ethersproject/units";
+import useGovernanceContract from "hooks/useGovernanceContract";
+import useSWR from "swr";
+import { useDataFromEventLogs } from "./useDataFromEventLogs";
+import useProposalCount from "./useProposalCount";
+
+const enumerateProposalState = (state) => {
+  const proposalStates = [
+    "pending",
+    "active",
+    "canceled",
+    "defeated",
+    "succeeded",
+    "queued",
+    "expired",
+    "executed",
+  ];
+
+  return proposalStates[state];
+};
+
+const getAllProposalData = (
+  govContract,
+  proposalIndexes,
+  formattedEvents
+) => async () => {
+  const allProposals = await Promise.all(
+    proposalIndexes.map(async (proposalID) => {
+      const res = await govContract.proposals(proposalID);
+
+      return {
+        result: res,
+      };
+    })
+  );
+
+  const allProposalStates = await Promise.all(
+    proposalIndexes.map(async (proposalID) => {
+      const res = await govContract.state(proposalID);
+
+      return {
+        result: res,
+      };
+    })
+  );
+
+  allProposals.reverse();
+  allProposalStates.reverse();
+
+  return allProposals
+    .filter((p, i) => {
+      return (
+        Boolean(p.result) &&
+        Boolean(allProposalStates[i]?.result) &&
+        Boolean(formattedEvents[i])
+      );
+    })
+    .map((p, i) => {
+      const formattedProposal = {
+        id: allProposals[i]?.result?.id.toString(),
+        title: formattedEvents[i].description?.split(/# |\n/g)[1] || "Untitled",
+        description:
+          formattedEvents[i].description?.split(/# /)[1] || "No description.",
+        proposer: allProposals[i]?.result?.proposer,
+        status:
+          enumerateProposalState(allProposalStates[i]?.result?.[0]) ??
+          "Undetermined",
+        forCount: parseFloat(
+          formatUnits(allProposals[i]?.result?.forVotes.toString(), 18)
+        ),
+        againstCount: parseFloat(
+          formatUnits(allProposals[i]?.result?.againstVotes.toString(), 18)
+        ),
+        startBlock: parseInt(allProposals[i]?.result?.startBlock?.toString()),
+        endBlock: parseInt(allProposals[i]?.result?.endBlock?.toString()),
+        details: formattedEvents[i].details,
+      };
+
+      return formattedProposal;
+    });
+};
+
+export default function useAllProposalData() {
+  const govContract = useGovernanceContract();
+
+  const { data: proposalCount } = useProposalCount();
+
+  const proposalIndexes = [];
+  for (let i = 1; i <= (proposalCount ?? 0); i++) {
+    proposalIndexes.push([i]);
+  }
+
+  // get metadata from past events
+  const { data: formattedEvents } = useDataFromEventLogs();
+
+  const shouldFetch = govContract && proposalCount && formattedEvents;
+
+  return useSWR(
+    shouldFetch
+      ? ["AllProposalData", govContract, proposalCount, formattedEvents]
+      : null,
+    getAllProposalData(govContract, proposalIndexes, formattedEvents)
+  );
+}
---
+++ hooks/governance/useProposalCount.js
@@ -0,0 +1,19 @@
+import useGovernanceContract from "hooks/useGovernanceContract";
+import useSWR from "swr";
+
+const getProposalCount = (contract) => async () => {
+  const res = await contract.proposalCount();
+
+  return parseInt(res);
+};
+
+export default function useProposalCount() {
+  const contract = useGovernanceContract();
+
+  const shouldFetch = Boolean(contract);
+
+  return useSWR(
+    shouldFetch ? ["ProposalCount", contract] : null,
+    getProposalCount(contract)
+  );
+}
