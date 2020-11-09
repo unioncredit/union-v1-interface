24250dbda2d63c0adcc4af539636c6cde280dbe9
Jeff Reiner <gone@hey.com>
2020-11-09 18:36:51 +0100

    Swapping to AddressZero to for 4

--- constants/governance.js
+++ constants/governance.js
@@ -1,15 +1,18 @@
 import governorAlpha from "./abis/governorAlpha.json";
 import comp from "./abis/comp.json";
+import { AddressZero } from "./variables";
 
 export const GOV_ADDRESS = {
-  1: "0xc0da01a04c3f3e0be433606045bb7017a7323e38",
+  1: AddressZero,
+  4: AddressZero,
   42: "0xfDc7E087395c8ba7EfFc9f888eD52379dB235023",
 };
 
 export const GOV_ABI = governorAlpha;
 
 export const GOV_TOKEN_ADDRESS = {
-  1: "0xc00e94cb662c3520282e6f5717214004a7f26888",
+  1: AddressZero,
+  4: AddressZero,
   42: "0x667242C6d755c302e753D31C16BC8d693b426127",
 };
 
