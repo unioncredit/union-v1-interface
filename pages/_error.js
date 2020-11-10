1f2b89d84eed1e219092efdbaea0e6a223322b1b
Jeff Reiner <gone@hey.com>
2020-11-10 13:17:36 +0100

    Make a custom erorr view

--- pages/_error.js
+++ pages/_error.js
@@ -1,7 +1,5 @@
-import Footer from "components/Footer";
-import Logo from "components/logo";
 import Head from "next/head";
-import Link from "next/link";
+import ErrorView from "views/error";
 
 export default function Error() {
   return (
@@ -12,41 +10,7 @@ export default function Error() {
         <meta name="twitter:title" content="Union" />
       </Head>
 
-      <header>
-        <nav className="border-b bg-white">
-          <div className="w-full mx-auto px-4 max-w-screen-xl-gutter">
-            <ul className="flex items-center justify-between">
-              <li className="py-4 h-20 flex items-center justify-start">
-                <Link href="/">
-                  <a>
-                    <Logo />
-                  </a>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </header>
-
-      <main className="flex-1">
-        <div className="my-8 md:my-10">
-          <div className="container">
-            <div className="text-center pt-32 pb-64">
-              <h1>Something went wrong</h1>
-
-              <p className="text-lg mt-4">
-                An unexpected error occurred{" "}
-                <a className="underline" href="mailto:support@union.finance">
-                  contact Support
-                </a>
-                .
-              </p>
-            </div>
-          </div>
-        </div>
-      </main>
-
-      <Footer />
+      <ErrorView />
     </div>
   );
 }
