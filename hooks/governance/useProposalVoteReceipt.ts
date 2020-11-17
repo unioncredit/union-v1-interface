4717466317035606e9a95dd818171b387af6d4f1
Jeff Reiner <gone@hey.com>
2020-11-17 17:19:58 +0100

    Update more ts

--- hooks/governance/useProposalVoteReceipt.ts
+++ hooks/governance/useProposalVoteReceipt.ts
@@ -1,24 +1,33 @@
+import type { BigNumber } from "@ethersproject/bignumber";
+import { Contract } from "@ethersproject/contracts";
 import { formatUnits } from "@ethersproject/units";
 import useGovernanceContract from "hooks/contracts/useGovernanceContract";
 import useSWR from "swr";
 
-const getProposalVoteReceipt = (govContract) => async (
-  _,
-  address,
-  proposalId
+const getProposalVoteReceipt = (govContract: Contract) => async (
+  _: any,
+  address: string,
+  proposalId: string
 ) => {
-  const receipt = await govContract.getReceipt(proposalId, address);
+  const receipt: {
+    hasVoted: boolean;
+    support: boolean;
+    votes: BigNumber;
+  } = await govContract.getReceipt(proposalId, address);
 
   const formattedReceipt = {
     hasVoted: receipt.hasVoted,
     support: receipt.support,
-    votes: formatUnits(receipt.votes.toString(), 18),
+    votes: formatUnits(receipt.votes, 18),
   };
 
   return formattedReceipt;
 };
 
-export default function useProposalVoteReceipt(address, proposalId) {
+export default function useProposalVoteReceipt(
+  address: string,
+  proposalId: string
+) {
   const govContract = useGovernanceContract();
 
   const shouldFetch = typeof address === "string" && proposalId && govContract;
