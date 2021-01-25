7da396bcd6bc3b77eacca8981b4798ad2301d051
arthcp <arth@matic.network>
2021-01-25 22:46:05 +0530

    new: StatsGrid and StatsCard components

--- views/stats/UnionToken.js
+++ views/stats/UnionToken.js
@@ -1,5 +1,7 @@
 import { Fragment } from "react";
 import StatsNavigation from "../../components/stats/StatsNavigation";
+import StatsCard from "../../components/stats/StatsCard";
+import StatsGrid from "../../components/stats/StatsGrid";
 
 export default function UnionTokenStatsView() {
   return (
@@ -22,17 +24,21 @@ export default function UnionTokenStatsView() {
 
       <section className="mb-8">
         <div className="container">
-          <ul className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
-            <li className="border rounded p-4 sm:p-6">
-              <p className="crop-snug leading-snug mb-4">Label</p>
-              <div className="flex items-center justify-between">
-                <p className="text-xl font-semibold crop-snug leading-snug">
-                  Value
-                </p>
-                <p className="text-sm leading-none">+100%</p>
-              </div>
-            </li>
-          </ul>
+          <StatsGrid>
+            <StatsCard label="Total Supply" value="100000" />
+            <StatsCard label="Reservoir Balance" value="10000" />
+            <StatsCard label="Comptroller Balance" value="10000" />
+            <StatsCard label="Annualized Inflation" value="20%" />
+            <StatsCard label="UPB" value="1000" />
+            <StatsCard label="Transfers" value="off" />
+            <StatsCard label="Total Borrowed" value="20000" />
+            <StatsCard label="Total Redeemable" value="20000" />
+            <StatsCard label="Total Reserves" value="200000" />
+            <StatsCard label="Defaulted Loan Amount" value="4000" />
+            <StatsCard label="Frozen Loan Amount" value="40000" />
+            <StatsCard label="uDAI Supply" value="400000" />
+            <StatsCard label="DAI/uDAI Exchange Rate" value="20" />
+          </StatsGrid>
         </div>
       </section>
     </Fragment>
