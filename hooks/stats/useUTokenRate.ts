ad3c609b8c216e2f221b378e3354af31b9c4c26d
arthcp <arth@matic.network>
2021-02-01 17:17:28 +0530

    fix: dai/udai exchange rate

--- hooks/stats/useUTokenRate.ts
+++ hooks/stats/useUTokenRate.ts
@@ -1,24 +1,22 @@
 import useUTokenContract from "hooks/contracts/useUTokenContract";
-import useUTokenDecimals from "hooks/useUTokenDecimals";
 import { formatUnits } from "@ethersproject/units";
 import { BigNumber } from "@ethersproject/bignumber";
 import { Contract } from "@ethersproject/contracts";
 import useSWR from "swr";
 
 const getUTokenRate = (uTokenContract: Contract) => async (
-  _: any,
-  decimals: BigNumber
+  _: any
 ) => {
-  const totalSupply: BigNumber = await uTokenContract.totalBorrows();
-  return formatUnits(totalSupply, decimals);
+  const uTokenRate: BigNumber = await uTokenContract.exchangeRateStored();
+  const decimals = BigNumber.from(18)
+  return formatUnits(uTokenRate, decimals);
 };
 
 export default function useUTokenRate() {
   const uTokenContract: Contract = useUTokenContract();
-  const { data: decimals } = useUTokenDecimals();
   const shouldFetch = !!uTokenContract;
   return useSWR(
-    shouldFetch ? ["uTokenRate", decimals] : null,
+    shouldFetch ? ["uTokenRate"] : null,
     getUTokenRate(uTokenContract)
   );
 }
