d8bc5a9be76eb3a37702e6e8216807a63836ec2c
arthcp <arth@matic.network>
2021-02-02 14:15:09 +0530

    chg: integrate governance stats

--- views/stats/governance.js
+++ views/stats/governance.js
@@ -2,8 +2,20 @@ import { Fragment } from "react";
 import StatsNavigation from "components/stats/StatsNavigation";
 import StatsCard from "components/stats/StatsCard";
 import StatsGrid from "components/stats/StatsGrid";
+import useProposalQuorum from "hooks/governance/useProposalQuorum";
+import useProposalThreshold from "hooks/governance/useProposalThreshold";
+import useVotingPeriod from "hooks/stats/useVotingPeriod";
+import useVotingDelay from "hooks/stats/useVotingDelay";
+import useTimelock from "hooks/stats/useTimelock";
+import format from "util/formatValue";
 
 export default function GovernanceStatsView() {
+  const { data: quorum } = useProposalQuorum();
+  const { data: threshold } = useProposalThreshold();
+  const { data: votingPeriod } = useVotingPeriod();
+  const { data: votingDelay } = useVotingDelay();
+  const { data: timelock } = useTimelock();
+
   return (
     <Fragment>
       <section className="mb-8">
@@ -25,11 +37,11 @@ export default function GovernanceStatsView() {
       <section className="mb-8">
         <div className="container">
           <StatsGrid>
-            <StatsCard label="Quorum" value="200000" />
-            <StatsCard label="Proposal Threshold" value="20000" />
-            <StatsCard label="Voting Period" value="30" />
-            <StatsCard label="Delay Period" value="50" />
-            <StatsCard label="Timelock" value="50" />
+            <StatsCard label="Quorum" value={format(quorum)} />
+            <StatsCard label="Proposal Threshold" value={format(threshold)} />
+            <StatsCard label="Voting Period" value={format(votingPeriod)} />
+            <StatsCard label="Delay Period" value={format(votingDelay)} />
+            <StatsCard label="Timelock" value={format(timelock)} />
           </StatsGrid>
         </div>
       </section>
