d2c7dd38703f895d7f88da18c414b48fad6f0fee
arthcp <arth@matic.network>
2021-02-17 18:16:04 +0530

    fix: function name

--- hooks/stats/assetManagerStats/useAssetManagerDAIBalance.ts
+++ hooks/stats/assetManagerStats/useAssetManagerDAIBalance.ts
@@ -2,7 +2,7 @@ import { ASSET_MANAGER_ADDRESSES, TOKENS } from "constants/variables";
 import useChainId from "hooks/useChainId";
 import useTokenBalanceOfAccount from "hooks/data/useTokenBalanceOfAccount";
 
-export default function useComptrollerUnionBalance() {
+export default function useAssetManagerDAIBalance() {
   const chainId = useChainId();
 
   return useTokenBalanceOfAccount(
