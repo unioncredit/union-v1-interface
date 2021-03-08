0f4f1deeac729b044eebd0a8bd648f0f59d0044a
arthcp <arth@matic.network>
2021-03-08 14:07:58 +0530

    chg: UPB label

--- views/stats/UnionToken.js
+++ views/stats/UnionToken.js
@@ -66,7 +66,7 @@ export default function UnionTokenStatsView() {
               }
             />
             <StatsCard
-              label="Actual UPB"
+              label="Inflation Per Block"
               value={
                 unionInflationPerBlock
                   ? formatDetailed(unionInflationPerBlock) + " Union"
