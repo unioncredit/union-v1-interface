cad4ebd24caa3591508b42ca6bceabc85ff5c536
twygod <twygod@gmail.com>
2021-05-11 15:11:56 +0800

    fix: grammatical errors.!minor

--- hooks/governance/useProposalCount.ts
+++ hooks/governance/useProposalCount.ts
@@ -3,9 +3,9 @@ import useGovernanceContract from "hooks/contracts/useGovernanceContract";
 import useSWR from "swr";
 
 const getProposalCount = (contract: Contract) => async () => {
-  console.log("getProposalCount start", parseInt(new Date().getTime() / 1000));
+  console.log("getProposalCount start", new Date().getTime() / 1000);
   const res = await contract.proposalCount();
-  console.log("getProposalCount end", parseInt(new Date().getTime() / 1000));
+  console.log("getProposalCount end", new Date().getTime() / 1000);
   console.log("proposalCount", res);
   return parseInt(res);
 };
