abf26ccca0bc8f41f5479087eafb7e587efb1757
twygod <twygod@gmail.com>
2021-07-09 17:30:15 +0800

    fix: Type definition error.

--- util/getIPFSAssetLink.ts
+++ util/getIPFSAssetLink.ts
@@ -1,4 +1,4 @@
-export default function getIPFSAssetLink(image: Object) {
+export default function getIPFSAssetLink(image: any) {
   return `https://ipfs.infura.io/ipfs/${
     image?.original?.src?.split("ipfs://")[1]
   }`;
