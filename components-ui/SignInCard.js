c495da03c44c60206ce7cd7f2eefb1ff62c9f331
Gerald <me@jacobford.co.uk>
2021-08-06 17:17:51 +0100

    feat: remove debugger

--- components-ui/SignInCard.js
+++ components-ui/SignInCard.js
@@ -35,7 +35,6 @@ const WalletOptions = ({
         );
 
         const handleSignIn = async () => {
-          debugger;
           setActivatingConnector(currentConnector);
 
           await activate(CONNECTORS[name]);
