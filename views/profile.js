579ecdb8375020ce6b9592265bb2b3004848fd76
Gerald <me@jacobford.co.uk>
2021-11-03 10:00:19 +0000

    fix: vouch modal to profile page

--- views/profile.js
+++ views/profile.js
@@ -27,6 +27,7 @@ import activityLabels from "util/activityLabels";
 import isHash from "util/isHash";
 import handleTxError from "util/handleTxError";
 import useDelegate from "hooks/payables/useDelegate";
+import { useVouchModal, VouchModalManager } from "components-ui/modals";
 
 export default function ProfileView({ address }) {
   const { account, library } = useWeb3React();
@@ -37,6 +38,8 @@ export default function ProfileView({ address }) {
   const { data: trustData = [] } = useTrustData(address);
   const { data: creditLimit = "0.0" } = useCreditLimit(address);
   const { data: votingWalletData } = useVotingWalletData(address);
+
+  const { open: openVouchModal } = useVouchModal();
 
   const {
     balanceOf = 0,
@@ -78,136 +81,144 @@ export default function ProfileView({ address }) {
   };
 
   return (
-    <View>
-      <Grid>
-        <Grid.Row justify="center">
-          <Grid.Col>
-            <Card mt="24px">
-              <Card.Body>
-                <Box direction="vertical" align="center">
-                  <Avatar size={56} address={address} />
-                  <Heading mt="8px" mb={0}>
-                    {name}
-                  </Heading>
-                  <Box align="center" justify="center" my="10px" fluid>
-                    <Dollar width="24px" />
-                    <Label as="p" grey={600} m={0} mr="8px">
-                      {creditLimitSummary} DAI
-                    </Label>
-                    <People width="24px" />
-                    <Label as="p" grey={600} m={0} mr="8px">
-                      {vouchData.length} Vouches
-                    </Label>
-                  </Box>
-                  <Box>
-                    <Badge
-                      label={
-                        <Copyable value={address}>
-                          {address.slice(0, 6)}
-                        </Copyable>
-                      }
-                      color="grey"
-                    />
-                  </Box>
-                  {vouchesForYou && (
-                    <Text mt="20px" mb={0} grey={500}>
-                      Vouching for you
-                    </Text>
-                  )}
-                  {vouchedForThem && (
-                    <Button
-                      fluid
-                      mt="20px"
-                      icon="vouch"
-                      label={`Vouch for ${name}`}
-                    />
-                  )}
-                  {account === address && (
-                    <>
-                      <Button
-                        as="a"
-                        target="_blank"
-                        href="https://app.ens.domains/"
-                        mt="32px"
-                        mb="8px"
-                        fluid
+    <>
+      <View>
+        <Grid>
+          <Grid.Row justify="center">
+            <Grid.Col>
+              <Card mt="24px">
+                <Card.Body>
+                  <Box direction="vertical" align="center">
+                    <Avatar size={56} address={address} />
+                    <Heading mt="8px" mb={0}>
+                      {name}
+                    </Heading>
+                    <Box align="center" justify="center" my="10px" fluid>
+                      <Dollar width="24px" />
+                      <Label as="p" grey={600} m={0} mr="8px">
+                        {creditLimitSummary} DAI
+                      </Label>
+                      <People width="24px" />
+                      <Label as="p" grey={600} m={0} mr="8px">
+                        {vouchData.length} Vouches
+                      </Label>
+                    </Box>
+                    <Box>
+                      <Badge
                         label={
-                          <>
-                            Get a custom ENS username
-                            <External />
-                          </>
+                          <Copyable value={address}>
+                            {address.slice(0, 6)}
+                          </Copyable>
                         }
+                        color="grey"
                       />
-                    </>
-                  )}
-                </Box>
-              </Card.Body>
-            </Card>
-            <Card mt="24px">
-              <Card.Body>
-                <Grid>
-                  <Grid.Row>
-                    <Grid.Col>
-                      <Heading m={0}>Reputation</Heading>
-                      <Text mt="24px" mb="12px">
-                        Wallet traits
+                    </Box>
+                    {vouchesForYou && (
+                      <Text mt="20px" mb={0} grey={500}>
+                        Vouching for you
                       </Text>
-                    </Grid.Col>
-                  </Grid.Row>
-                  <Grid.Row>
-                    <Grid.Col>
-                      <Stat
-                        label="receiving vouches from"
-                        value={`${vouchData.length} accounts`}
+                    )}
+                    {vouchedForThem && (
+                      <Button
+                        fluid
+                        mt="20px"
+                        icon="vouch"
+                        onClick={openVouchModal}
+                        label={`Vouch for ${name}`}
                       />
-                    </Grid.Col>
-                    <Grid.Col>
-                      <Stat
-                        label="Vouched for others"
-                        value={`${trustData.length} times`}
-                      />
-                    </Grid.Col>
-                  </Grid.Row>
-                </Grid>
-                <Divider mt="24px" />
-                <Grid>
-                  <Grid.Row>
-                    <Grid.Col>
-                      <Heading mt="24px">Governance</Heading>
-                    </Grid.Col>
-                  </Grid.Row>
-                  <Grid.Row>
-                    <Grid.Col>
-                      <Stat label="Total Votes" value={format(currentVotes)} />
-                    </Grid.Col>
-                    <Grid.Col>
-                      <Stat label="Union Balance" value={format(balanceOf)} />
-                    </Grid.Col>
-                    <Grid.Col>
-                      <Stat
-                        label="From others"
-                        value={format(votesDelegated)}
-                      />
-                    </Grid.Col>
-                  </Grid.Row>
-                  {address && address !== account && library && (
+                    )}
+                    {account === address && (
+                      <>
+                        <Button
+                          as="a"
+                          target="_blank"
+                          href="https://app.ens.domains/"
+                          mt="32px"
+                          mb="8px"
+                          fluid
+                          label={
+                            <>
+                              Get a custom ENS username
+                              <External />
+                            </>
+                          }
+                        />
+                      </>
+                    )}
+                  </Box>
+                </Card.Body>
+              </Card>
+              <Card mt="24px">
+                <Card.Body>
+                  <Grid>
                     <Grid.Row>
                       <Grid.Col>
-                        <Button
-                          mt="24px"
-                          variant="secondary"
-                          label={`Delegate votes to ${name}`}
-                          onClick={handleDelegation}
+                        <Heading m={0}>Reputation</Heading>
+                        <Text mt="24px" mb="12px">
+                          Wallet traits
+                        </Text>
+                      </Grid.Col>
+                    </Grid.Row>
+                    <Grid.Row>
+                      <Grid.Col>
+                        <Stat
+                          label="receiving vouches from"
+                          value={`${vouchData.length} accounts`}
+                        />
+                      </Grid.Col>
+                      <Grid.Col>
+                        <Stat
+                          label="Vouched for others"
+                          value={`${trustData.length} times`}
                         />
                       </Grid.Col>
                     </Grid.Row>
-                  )}
-                </Grid>
-              </Card.Body>
-            </Card>
-          </Grid.Col>
-        </Grid.Row>
-      </Grid>
-    </View>
+                  </Grid>
+                  <Divider mt="24px" />
+                  <Grid>
+                    <Grid.Row>
+                      <Grid.Col>
+                        <Heading mt="24px">Governance</Heading>
+                      </Grid.Col>
+                    </Grid.Row>
+                    <Grid.Row>
+                      <Grid.Col>
+                        <Stat
+                          label="Total Votes"
+                          value={format(currentVotes)}
+                        />
+                      </Grid.Col>
+                      <Grid.Col>
+                        <Stat label="Union Balance" value={format(balanceOf)} />
+                      </Grid.Col>
+                      <Grid.Col>
+                        <Stat
+                          label="From others"
+                          value={format(votesDelegated)}
+                        />
+                      </Grid.Col>
+                    </Grid.Row>
+                    {address && address !== account && library && (
+                      <Grid.Row>
+                        <Grid.Col>
+                          <Button
+                            mt="24px"
+                            variant="secondary"
+                            label={`Delegate votes to ${name}`}
+                            onClick={handleDelegation}
+                          />
+                        </Grid.Col>
+                      </Grid.Row>
+                    )}
+                  </Grid>
+                </Card.Body>
+              </Card>
+            </Grid.Col>
+          </Grid.Row>
+        </Grid>
+      </View>
+
+      <VouchModalManager />
+    </>
   );
 }
