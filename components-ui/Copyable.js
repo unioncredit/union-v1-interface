1f87489dfc8bc3eb5eda032c117da02971cb223b
Gerald <me@jacobford.co.uk>
2021-11-09 09:31:31 +0000

    feat: copyable copied text

--- components-ui/Copyable.js
+++ components-ui/Copyable.js
@@ -4,7 +4,7 @@ export function Copyable({ value, children }) {
   const [isCopied, copy] = useCopy(2000);
   return (
     <span onClick={() => copy(value)} className="copyable">
-      {isCopied ? "copied!" : children}
+      {isCopied ? "Copied" : children}
     </span>
   );
 }
