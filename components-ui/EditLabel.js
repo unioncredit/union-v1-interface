2a3b95e9349bad637dea3c5d1652b86e80442008
Gerald <me@jacobford.co.uk>
2021-11-09 20:21:45 +0000

    feat: edit label enter and save

--- components-ui/EditLabel.js
+++ components-ui/EditLabel.js
@@ -1,22 +1,44 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useCallback } from "react";
 import { Box, Label, Text, Button } from "union-ui";
 import useAddressLabels from "hooks/useAddressLabels";
+import setEndOfContentEditable from "util/setEndOfContentEditable";
 
 export function EditLabel({ address }) {
   const labelEl = useRef(null);
   const [editting, setEditting] = useState(false);
-
   const { getLabel, setLabel } = useAddressLabels();
 
-  const handleEdit = () => {
-    setEditting(true);
-    setTimeout(() => labelEl.current.focus(), 0);
-  };
+  const label = getLabel(address);
 
-  const handleSave = () => {
+  const handleSave = useCallback(() => {
     const label = labelEl.current.innerText;
     setLabel(address, label);
     setEditting(false);
+    document.removeEventListener("keydown", ignoreEnterKey);
+  }, [setLabel, setEditting]);
+
+  const ignoreEnterKey = useCallback(
+    (event) => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        handleSave();
+        return;
+      }
+    },
+    [handleSave]
+  );
+
+  const handleEdit = () => {
+    setEditting(true);
+    setTimeout(() => {
+      setEndOfContentEditable(labelEl.current);
+    }, 0);
+    document.addEventListener("keydown", ignoreEnterKey);
+  };
+
+  const handleCancel = () => {
+    setEditting(false);
+    labelEl.current.textContent = label;
   };
 
   return (
@@ -32,7 +54,7 @@ export function EditLabel({ address }) {
           ref={labelEl}
           className="editable-name"
         >
-          {getLabel(address) || ""}
+          {label || ""}
         </Text>
       </Box>
       <Box>
@@ -48,7 +70,7 @@ export function EditLabel({ address }) {
               ml="4px"
               variant="pill"
               label="Cancel"
-              onClick={() => setEditting(false)}
+              onClick={handleCancel}
             />
           </>
         ) : (
