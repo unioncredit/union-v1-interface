9d8be7db83b9c17fcc796ef2ad824097595ac428
Gerald <me@jacobford.co.uk>
2021-11-17 14:55:51 +0000

    feat: update avatar

--- components-ui/Avatar.js
+++ components-ui/Avatar.js
@@ -1,23 +1,21 @@
 import { useState } from "react";
 import { Avatar as UIAvatar } from "union-ui";
-import Identicon from "react-identicons";
 import useENSName from "hooks/useENSName";
 import useENSImage from "hooks/useENSImage";
+import makeBlockie from "ethereum-blockies-base64";
 
 export function Avatar({ address, size }) {
   const [error, setError] = useState(false);
   const ENSName = useENSName(address);
   const image = useENSImage(ENSName);
+  const blockie = makeBlockie(address);
 
-  const fg = `#${address.slice(2, 8)}`;
-  const bg = `${fg}55`;
-
-  const hasENSImage = !!image;
-
-  return hasENSImage && !error ? (
-    <UIAvatar size={size} src={image} onError={() => setError(true)} />
-  ) : (
-    <Identicon size={size} string={address} fg={fg} bg={bg} />
+  return (
+    <UIAvatar
+      size={size}
+      src={error ? blockie : image || blockie}
+      onError={() => setError(true)}
+    />
   );
 }
 
