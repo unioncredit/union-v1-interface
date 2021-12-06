eea222683051e6adb5f1530cde08f7b0044fc0c6
Gerald <me@jacobford.co.uk>
2021-12-06 22:35:57 +0000

    fix(UNI-390): Menu doesn't properly hide get started or change when lower width

--- components-ui/View.js
+++ components-ui/View.js
@@ -1,9 +1,11 @@
+import { useMemo } from "react";
 import { Box, ToggleMenu, TabNav } from "union-ui";
 import { useRouter } from "next/router";
 import { useWeb3React } from "@web3-react/core";
 import { TabLink, Link, OverdueAlert } from "components-ui";
 import useIsMobile from "hooks/useIsMobile";
 import { navItems } from "constants/app";
+import useIsMember from "hooks/data/useIsMember";
 
 import styles from "./view.module.css";
 
@@ -12,18 +14,25 @@ export function View({ children, tabItems }) {
 
   const router = useRouter();
   const { account } = useWeb3React();
+  const { data: isMember } = useIsMember();
 
   const tabItemLinks =
     tabItems?.length > 0
       ? tabItems.map((item) => ({ ...item, as: TabLink }))
       : [];
 
-  const navItemLinks = navItems.map((item) => ({
-    ...item,
-    as: Link,
-    active: item.pathname === router.pathname,
-    href: item.id === "profile" ? `/profile/${account}` : item.pathname,
-  }));
+  const navItemLinks = useMemo(() => {
+    if (!isMember) {
+      return [];
+    }
+
+    return navItems.slice(1).map((item) => ({
+      ...item,
+      as: Link,
+      active: item.pathname === router.pathname,
+      href: item.id === "profile" ? `/profile/${account}` : item.pathname,
+    }));
+  }, []);
 
   const initialTab = tabItemLinks.findIndex(
     (item) => item.href === router.pathname
