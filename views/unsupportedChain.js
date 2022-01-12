f7ad2f815021a2d088523403617487bd60ec5efc
Gerald <me@jacobford.co.uk>
2022-01-12 11:26:10 +0000

    feat: network urls

--- views/unsupportedChain.js
+++ views/unsupportedChain.js
@@ -10,6 +10,7 @@ import {
 } from "union-ui";
 import { useState } from "react";
 import { switchChain, options } from "util/switchChain";
+import { networks } from "lib/connectors";
 
 export default function UnsupportedChainView() {
   const [loading, setIsLoading] = useState(false);
@@ -26,7 +27,7 @@ export default function UnsupportedChainView() {
   };
 
   return (
-    <Box justify="center" align="center" direction="vertical" fluid>
+    <Box align="center" direction="vertical" fluid>
       <Box mt="48px" className="hide-lt-600" />
       <Heading
         mb="4px"
@@ -44,36 +45,45 @@ export default function UnsupportedChainView() {
       <Grid>
         <Grid.Row justify="center">
           <Grid.Col md={12} lg={9}>
-            <Grid.Row>
-              {options.map((props) => {
-                const { label, value, imageSrc, description, buttonVariant } =
-                  props;
-                return (
-                  <Grid.Col xs={12} md={6} key={label}>
-                    <Card mb="24px">
-                      <Card.Body>
-                        <Box justify="center" mb="24px">
-                          <Avatar size={48} src={imageSrc} />
-                        </Box>
-                        <Text grey={800} weight="medium" as="h2" align="center">
-                          {label}
-                        </Text>
-                        <Label as="p" align="center">
-                          {description}
-                        </Label>
-                        <Button
-                          fluid
-                          mt="24px"
-                          variant={buttonVariant}
-                          loading={loading === buttonVariant}
-                          label={`Switch to ${value}`}
-                          onClick={handleChangeNetwork(props)}
-                        />
-                      </Card.Body>
-                    </Card>
-                  </Grid.Col>
-                );
-              })}
+            <Grid.Row justify="center">
+              {options
+                .filter((option) =>
+                  Object.keys(networks).includes(String(option.chainId))
+                )
+                .map((props) => {
+                  const { label, value, imageSrc, description, buttonVariant } =
+                    props;
+                  return (
+                    <Grid.Col xs={12} md={6} key={label}>
+                      <Card mb="24px">
+                        <Card.Body>
+                          <Box justify="center" mb="24px">
+                            <Avatar size={48} src={imageSrc} />
+                          </Box>
+                          <Text
+                            grey={800}
+                            weight="medium"
+                            as="h2"
+                            align="center"
+                          >
+                            {label}
+                          </Text>
+                          <Label as="p" align="center">
+                            {description}
+                          </Label>
+                          <Button
+                            fluid
+                            mt="24px"
+                            variant={buttonVariant}
+                            loading={loading === buttonVariant}
+                            label={`Switch to ${value}`}
+                            onClick={handleChangeNetwork(props)}
+                          />
+                        </Card.Body>
+                      </Card>
+                    </Grid.Col>
+                  );
+                })}
             </Grid.Row>
           </Grid.Col>
         </Grid.Row>
