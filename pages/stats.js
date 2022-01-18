5b15ca0ba9cfe8d2465fa9c58d6b3bebdeb728ed
Gerald <me@jacobford.co.uk>
2022-01-18 10:51:20 +0000

    fix: stats page login button not working

--- pages/stats.js
+++ pages/stats.js
@@ -1,11 +1,15 @@
+import LoggedOutView from "views/loggedOut";
 import StatsView from "views/stats";
+import { useForceConnect } from "hooks/useForceConnect";
 import { PageHead } from "components-ui";
 
 export default function StatsPage() {
+  const [forceConnect] = useForceConnect();
+
   return (
     <>
       <PageHead title="Stats | Union" />
-      <StatsView />
+      {forceConnect ? <LoggedOutView /> : <StatsView />}
     </>
   );
 }
