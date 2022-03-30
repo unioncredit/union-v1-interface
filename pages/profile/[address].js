913aeaaaf7f0275325a0abab81bdd103f7f5062d
twygod <twygod@gmail.com>
2022-03-30 14:34:53 +0800

    new: Add ens suffix to profile URL.

--- pages/profile/[address].js
+++ pages/profile/[address].js
@@ -2,12 +2,20 @@ import Head from "next/head";
 import ProfileView from "views/profile";
 import { PageHead } from "components-ui";
 import { useForceConnect } from "hooks/useForceConnect";
+import useENS from "hooks/useENS";
 import LoggedOutView from "views/loggedOut";
+import { isAddress } from "@ethersproject/address";
 
 export default function ProfilePage({ params, host }) {
   const [forceConnect] = useForceConnect();
-
   const { address } = params;
+  const hasAddress = isAddress(address);
+  let ENSAddress;
+  if (!hasAddress) {
+    const ens = useENS(address);
+    ENSAddress = ens.address;
+  }
+  const userAddress = hasAddress ? address : ENSAddress;
 
   return (
     <>
@@ -16,23 +24,29 @@ export default function ProfilePage({ params, host }) {
         <meta
           key="og:image"
           property="og:image"
-          content={`https://${host}/api/og/profile?address=${address}`}
+          content={`https://${host}/api/og/profile?address=${userAddress}`}
         />
         <meta
           key="twitter:image"
           property="twitter:image"
-          content={`https://${host}/api/og/profile?address=${address}`}
+          content={`https://${host}/api/og/profile?address=${userAddress}`}
         />
         <meta
           property="twitter:title"
           key="twitter:title"
-          content={`Union Member ${address}`}
+          content={`Union Member ${userAddress}`}
         />
       </Head>
 
       {address && (
         <>
-          {forceConnect ? <LoggedOutView /> : <ProfileView address={address} />}
+          {forceConnect ? (
+            <LoggedOutView />
+          ) : userAddress ? (
+            <ProfileView address={userAddress} />
+          ) : (
+            <></>
+          )}
         </>
       )}
     </>
