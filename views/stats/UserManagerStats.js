4c08758ab2f9a34813dfae8bf42171f33a4f19c5
Gerald <xyz@gerald.host>
2022-04-08 23:37:47 +0100

    feat: re-add missing max stake to stats

--- views/stats/UserManagerStats.js
+++ views/stats/UserManagerStats.js
@@ -1,16 +1,22 @@
 import { Stat, Grid } from "@unioncredit/ui";
+import { formatEther } from "ethers/lib/utils";
 
 import useUserManagerStats from "hooks/stats/userManagerStats";
 import { daiValue } from "./values";
 
 function useUserManagerStatsView() {
-  const { totalStakedDAI, totalFrozenStake, effectiveTotalStake } =
-    useUserManagerStats();
+  const {
+    maxStakeAmount,
+    totalStakedDAI,
+    totalFrozenStake,
+    effectiveTotalStake,
+  } = useUserManagerStats();
 
   return [
     { label: "Total Staked DAI", value: daiValue(totalStakedDAI) },
     { label: "Effective Total Stakes", value: daiValue(effectiveTotalStake) },
     { label: "Total Frozen Stake", value: daiValue(totalFrozenStake) },
+    { label: "Max Stake Amount", value: daiValue(formatEther(maxStakeAmount)) },
   ];
 }
 
