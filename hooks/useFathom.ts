761ff58a4db1f293c7efa60ebcdae8537c994567
Gerald <gm@gerald.host>
2022-05-20 16:45:06 +0100

    fix: exact domain instead of wildcard

--- hooks/useFathom.ts
+++ hooks/useFathom.ts
@@ -1,13 +1,18 @@
 import * as Fathom from "fathom-client";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { networkAppUrlsProd } from "lib/connectors";
+
+const domains = Object.values(networkAppUrlsProd).map((url: string) =>
+  url.replace("https://", "")
+);
 
 export default function useFathom() {
   const router = useRouter();
 
   useEffect(() => {
     Fathom.load(process.env.NEXT_PUBLIC_FATHOM_ID, {
-      includedDomains: ["*.union.finance"],
+      includedDomains: ["*.union.finance", ...domains],
     });
 
     function onRouteChangeComplete() {
