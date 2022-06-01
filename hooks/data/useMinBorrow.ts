c02b165fed91850be99622d368e4cdd48ba4a6ca
Gerald <gm@gerald.host>
2022-06-01 13:12:26 +0100

    feat: remove unused ts files

--- hooks/data/useMinBorrow.ts
+++ hooks/data/useMinBorrow.ts
@@ -1,4 +1,3 @@
-import useDAIDecimals from "hooks/useDAIDecimals";
 import { formatUnits } from "@ethersproject/units";
 import { BigNumber } from "@ethersproject/bignumber";
 import { Contract } from "@ethersproject/contracts";
@@ -6,21 +5,22 @@ import useSWR from "swr";
 import useUTokenContract from "hooks/contracts/useUTokenContract";
 import useReadProvider from "hooks/useReadProvider";
 
+const daiDecimals = 18;
+
 const getMinBorrow =
-  (uTokenContract: Contract) => async (_: any, decimals: BigNumber) => {
+  (uTokenContract: Contract) => async (_, decimals: BigNumber) => {
     const minBorrow: BigNumber = await uTokenContract.minBorrow();
     return Number(formatUnits(minBorrow, decimals));
   };
 
 export default function useMinBorrow() {
   const readProvider = useReadProvider();
-  const uTokenContract: Contract = useUTokenContract(readProvider);
-  const { data: decimals } = useDAIDecimals();
+  const uTokenContract = useUTokenContract(readProvider);
 
   const shouldFetch = !!uTokenContract;
 
   return useSWR(
-    shouldFetch ? ["minBorrow", decimals] : null,
+    shouldFetch ? ["minBorrow", daiDecimals] : null,
     getMinBorrow(uTokenContract)
   );
 }
