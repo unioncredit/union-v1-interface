3de4ef1b42217634996b0ef918dfe323d7c92aa8
Gerald <gm@gerald.host>
2022-06-29 12:08:22 +0100

    fix: fathom firing events

--- src/index.js
+++ src/index.js
@@ -1,20 +1,57 @@
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import * as Fathom from "fathom-client";
 import { SWRConfig } from "swr";
 import { Web3ReactProvider } from "@web3-react/core";
-import ErrorBoundary from "components-ui/ErrorBoundary";
+import { createBrowserHistory } from "history";
+import { Router } from "react-router-dom";
+
+import App from "./App";
 import { Notifications } from "components-ui";
+import ErrorBoundary from "components-ui/ErrorBoundary";
 import getLibrary from "lib/getLibrary";
+import { networkAppUrlsProd } from "lib/connectors";
 import UnsuportedChainProvider from "providers/UnsupportedChain";
 import Error from "pages/error";
 
 import "./index.scss";
 
+const BrowserHistory = createBrowserHistory();
+
+const domains = Object.values(networkAppUrlsProd).map((url) =>
+  url.replace("https://", "")
+);
+
+const BrowserRouter = ({ history, ...props }) => {
+  const [state, setState] = useState({
+    action: history.action,
+    location: history.location,
+  });
+
+  useLayoutEffect(() => {
+    Fathom.load(process.env.NEXT_PUBLIC_FATHOM_ID, {
+      includedDomains: ["*.union.finance", ...domains],
+    });
+
+    history.listen(({ action, location }) => {
+      setState({ action, location });
+      Fathom.trackPageview();
+    });
+  }, [history]);
+
+  return (
+    <Router
+      {...props}
+      location={state.location}
+      navigationType={state.action}
+      navigator={history}
+    />
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter history={BrowserHistory}>
     <UnsuportedChainProvider chainIds={[]}>
       <ErrorBoundary fallback={<Error />}>
         <SWRConfig
