9ef0d2c81103e374b1dd794a7aa8355ce856d6e8
twygod <twygod@gmail.com>
2022-07-04 15:40:24 +0800

    chg: update discord links.

--- src/constants/app.js
+++ src/constants/app.js
@@ -16,7 +16,7 @@ export const links = {
   website: "https://union.finance/",
   governance: "https://union.finance/governance",
   docs: "https://docs.union.finance/",
-  discord: "https://discord.gg/4re6YJdgKZ",
+  discord: "https://discord.gg/DWF29rjRJH",
   github: "https://github.com/unioncredit",
   data: "https://data.union.finance",
 };
