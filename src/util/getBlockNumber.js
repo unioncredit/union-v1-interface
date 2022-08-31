008adb8b620c81333d534ef392ccabaa7a05d3ed
Gerald <gm@gerald.host>
2022-09-01 00:10:41 +0100

    fix: get block number on arbitrum

--- src/util/getBlockNumber.js
+++ src/util/getBlockNumber.js
@@ -1,10 +1,13 @@
 export default async function getBlockNumber(library) {
   const currentBlockNumber = await library.getBlockNumber();
   const block = await library.getBlock(currentBlockNumber);
-  const resp = await library.provider.send("eth_getTransactionReceipt", [
-    block.transactions[0],
-  ]);
+
+  const args = ["eth_getTransactionReceipt", [block.transactions[0]]];
+  const resp = library.send
+    ? await library.send(...args)
+    : await library.provider.send(...args);
 
   // Supports ARBITRUM which returns L1 blockNumber as `l1BlockNumber`
-  return resp.result.l1BlockNumber || resp.result.blockNumber;
+  const result = resp.result || resp;
+  return result.l1BlockNumber || result.blockNumber;
 }
