12f8bb9b5926657ba6cba833e8bd450ebf38e844
twygod <twygod@gmail.com>
2022-10-26 16:31:11 +0800

    chg: remove multicall check.

--- src/hooks/data/useStakeData.js
+++ src/hooks/data/useStakeData.js
@@ -4,10 +4,9 @@ import { BigNumber } from "@ethersproject/bignumber";
 import useToken from "hooks/useToken";
 import useUserManager from "hooks/contracts/useUserManager";
 import useMulticall from "hooks/useMulticall";
-import { MULTICALL_ADDRESSES } from "constants/variables";
 
-function fetchStakeData(userManager) {
-  return async function (_, account, chainId, multicall) {
+function fetchStakeData(userManager, multicall) {
+  return async function (_, account) {
     const totalStake = await userManager.getStakerBalance(account);
     const totalLocked = await userManager.getTotalLockedStake(account);
     const totalFrozen = await userManager.getTotalFrozenAmount(account);
@@ -15,24 +14,17 @@ function fetchStakeData(userManager) {
 
     const borrowers = await userManager.getBorrowerAddresses(account);
     let totalMemberFrozen = BigNumber.from(0);
-    if (MULTICALL_ADDRESSES[chainId]) {
-      const calls = borrowers.map((borrower) => {
-        return {
-          address: userManager.address,
-          name: "memberFrozen",
-          params: [borrower],
-          itf: userManager.interface,
-        };
-      });
-      const [resp] = await Promise.all([await multicall(calls)]);
-      for (let i = 0; i < resp.length; i++) {
-        totalMemberFrozen = totalMemberFrozen.add(resp[i][0]);
-      }
-    } else {
-      for (let i = 0; i < borrowers.length; i++) {
-        const defaulted = await userManager.memberFrozen(borrowers[i]);
-        totalMemberFrozen = totalMemberFrozen.add(defaulted);
-      }
+    const calls = borrowers.map((borrower) => {
+      return {
+        address: userManager.address,
+        name: "memberFrozen",
+        params: [borrower],
+        itf: userManager.interface,
+      };
+    });
+    const resp = await multicall(calls);
+    for (let i = 0; i < resp.length; i++) {
+      totalMemberFrozen = totalMemberFrozen.add(resp[i][0]);
     }
 
     return {
@@ -47,7 +39,7 @@ function fetchStakeData(userManager) {
 }
 
 export default function useStakeData() {
-  const { account, chainId } = useWeb3React();
+  const { account } = useWeb3React();
   const DAI = useToken("DAI");
   const multicall = useMulticall();
   const userManager = useUserManager(DAI);
@@ -55,7 +47,7 @@ export default function useStakeData() {
   const shouldFetch = userManager && account;
 
   return useSWR(
-    shouldFetch ? ["useStakeData", account, chainId, multicall] : null,
-    fetchStakeData(userManager)
+    shouldFetch ? ["useStakeData", account] : null,
+    fetchStakeData(userManager, multicall)
   );
 }
